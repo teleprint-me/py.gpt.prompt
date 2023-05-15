0e144801497a6f861a56eac7440f2fbb0749d3a7
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-15 03:52:59 -0400

    refactor: Add token module for token management

---
+++ pygptprompt/token.py
@@ -0,0 +1,60 @@
+import tiktoken
+from tiktoken import Encoding
+
+
+def get_token_limit(model_name: str, max_tokens: int) -> tuple[Encoding, int]:
+    # gpt-3.5-turbo context window: `upper_limit = 4096 - max_tokens`
+    # gpt-4 context window: `upper_limit = 8192 - max_tokens`
+    encoding = tiktoken.encoding_for_model(model_name)
+    if model_name == "gpt-3.5-turbo":
+        upper_limit = 4096 - max_tokens
+    else:
+        upper_limit = 8192 - max_tokens
+    return (encoding, upper_limit)
+
+
+def get_token_count(encoding_name: str, messages: list[dict[str, str]]) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    total_tokens = 0
+
+    for message in messages:
+        if "content" in message:
+            string = message["content"]
+            total_tokens += len(encoding.encode(string))
+
+    return total_tokens
+
+
+def truncate_messages_for_token_limit(
+    messages: list[dict[str, str]],
+    new_message: dict[str, str],
+    upper_limit: int,
+    encoding: Encoding,
+) -> list[dict[str, str]]:
+    # Calculate the token count for the new message
+    new_message_token_count = get_token_count(encoding.name, [new_message])
+
+    # If the new message is too long to fit into the context window,
+    # print an error and return the original messages
+    if new_message_token_count > upper_limit:
+        print(
+            f"MessageError: Message too long ({new_message_token_count} tokens). Maximum is {upper_limit} tokens."
+        )
+        return messages
+
+    # Add the new message to the messages list
+    messages.append(new_message)
+
+    # If the total token count is within the limit, return the messages
+    total_token_count = get_token_count(encoding.name, messages)
+    if total_token_count <= upper_limit:
+        return messages
+
+    # Otherwise, remove the oldest messages until the total is within the limit
+    while total_token_count > upper_limit:
+        # Remove the oldest message (after the initial system message)
+        messages.pop(1)
+        total_token_count = get_token_count(encoding.name, messages)
+
+    return messages
