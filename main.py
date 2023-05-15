d7d0378af419a3a4db4ec6d07325f30985260076
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-15 03:53:41 -0400

    refactor: Remove token management and use config

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from dotenv import load_dotenv
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 
-from pygptprompt.command import handle_command
+from pygptprompt.command import config, handle_command
 from pygptprompt.openai import OpenAI
+from pygptprompt.token import (
+    get_token_count,
+    get_token_limit,
+    truncate_messages_for_token_limit,
+)
 
 load_dotenv()
 
@@ -16,41 +21,19 @@ def prompt_continuation(width, line_number, is_soft_wrap):
     # Or: return [('', '.' * width)]
 
 
-def get_token_count(encoding_name: str, messages: list[dict[str, str]]) -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.get_encoding(encoding_name)
-    total_tokens = 0
-
-    for message in messages:
-        if "content" in message:
-            string = message["content"]
-            total_tokens += len(encoding.encode(string))
-
-    return total_tokens
-
-
 def main():
     # Usage
-    max_tokens = 1024
-    model = "gpt-3.5-turbo"
-    api_key = os.getenv("OPENAI_API_KEY") or ""
+    max_tokens: int = config["max_tokens"]
+    model: str = config["model"]
+    api_key: str = os.getenv("OPENAI_API_KEY") or ""
     openai = OpenAI(api_key)
 
     # Initialize the conversation with a system message
-    messages = [
-        {
-            "role": "system",
-            "content": f"Your name is {model}. You are a pair programming assistant. You have access to special commands. Use `/help` for more information. You and the User are both allowed to execute commands listed in the `/help` output.",
-        }
-    ]
+    messages: list[dict[str, str]] = [config["system_message"]]
 
     # gpt-3.5-turbo context window: `upper_limit = 4096 - max_tokens`
     # gpt-4 context window: `upper_limit = 8192 - max_tokens`
-    encoding = tiktoken.encoding_for_model(model)
-    if model == "gpt-3.5-turbo":
-        upper_limit = 4096 - max_tokens
-    else:
-        upper_limit = 8192 - max_tokens
+    encoding, upper_limit = get_token_limit(model, max_tokens)
 
     while True:
         # Calculate the total number of tokens enqueued
@@ -81,13 +64,15 @@ def main():
             print(command_response)
             user_message = f"{user_message}\n{command_response}"
 
-        # Enqueue the user message to the conversation
-        messages.append({"role": "user", "content": user_message})
-
-        # Dequeue older messages to prevent overflow.
+        # Add the user message to the conversation
+        #   - Dequeue older messages to prevent overflow.
         #   - pop the second element to preserve the system prompt.
-        while token_count >= upper_limit:
-            messages.pop(1)
+        messages = truncate_messages_for_token_limit(
+            messages,
+            {"role": "user", "content": user_message},
+            upper_limit,
+            encoding,
+        )
 
         # Use a prompt to identify GPT's output
         print("\nGPT:", end=" ")
@@ -108,8 +93,13 @@ def main():
                 assistant_content = f"{assistant_content}\n{command_response}"
                 assistant_message = {"role": "assistant", "content": assistant_content}
 
-            # Enqueue the assistant message.
-            messages.append(assistant_message)
+            # Add the assistant message to the conversation
+            messages = truncate_messages_for_token_limit(
+                messages,
+                assistant_message,
+                upper_limit,
+                encoding,
+            )
 
         print("\n")  # output newline characters
 
