31832c7c55e727bded1c38dc381751e8abcd5e54
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-15 19:30:43 -0400

    refactor: Apply updated changes
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
+from typing import Any
 
+import tiktoken
 from dotenv import load_dotenv
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
+from tiktoken import Encoding
 
-from pygptprompt.command import config, handle_command
+from pygptprompt.command.factory import command_factory
+from pygptprompt.config import get_config
 from pygptprompt.format import print_bold
 from pygptprompt.openai import OpenAI
 from pygptprompt.session import (
@@ -21,6 +25,10 @@ from pygptprompt.token import (
 
 load_dotenv()
 
+#
+# TODO: Refactor to implement separation of concerns
+#
+
 
 def prompt_continuation(width, line_number, is_soft_wrap):
     return ">" * width
@@ -29,55 +37,78 @@ def prompt_continuation(width, line_number, is_soft_wrap):
 
 def main():
     # Usage
+
+    #
+    # TODO: Prompt for configuration file path.
+    #
+
+    # Configuration defaults to `./config.json`.
+    config: dict[str, Any] = get_config()
     max_tokens: int = config["max_tokens"]
     model: str = config["model"]
-    temperature = config["temperature"]
+    temperature: float = config["temperature"]
     api_key: str = os.getenv("OPENAI_API_KEY") or ""
-    openai = OpenAI(api_key)
+    openai: OpenAI = OpenAI(api_key)
 
     # Ask the user to enter a session name or choose an existing one
     create_sessions_directory()
-    session_name = name_session()
-    messages = read_session(session_name) or [config["system_message"]]
-    print()
+    session_name: str = name_session()
+    messages: list[dict[str, str]] = read_session(session_name) or [
+        config["system_message"]
+    ]
 
     # Print message history
     for message in messages:
+        print()  # Add padding to command line
         print_bold(message["role"])
         print(message["content"])
 
+    print()
+
     # gpt-3.5-turbo context window: `upper_limit = 4096 - max_tokens`
     # gpt-4 context window: `upper_limit = 8192 - max_tokens`
-    encoding, upper_limit = get_token_limit(model, max_tokens)
+    encoding: Encoding = tiktoken.encoding_for_model(model)
+    upper_limit: int = get_token_limit(model, max_tokens)
 
     while True:
         # Calculate the total number of tokens enqueued
-        token_count = get_token_count(encoding.name, messages=messages)
+        token_count: int = get_token_count(encoding.name, messages=messages)
         print(f"Consumed {token_count} tokens.\n")
 
         # Ask the user for their message
         try:
             print_bold("user")
-            user_message = prompt(
+            user_message: str = prompt(
                 "> ",
                 multiline=True,
                 prompt_continuation=prompt_continuation,
                 history=FileHistory(f"sessions/{session_name}.history"),
             )
         except (KeyboardInterrupt, EOFError):
+            save_session(session_name, messages)  # Auto save session
             exit()
 
         # Block and prompt user again if the input is empty
         if not user_message:
             continue
 
+        #
+        # TODO: Handle meta sessions
+        #   - Allow user to manage sessions from within sessions
+        #
+
+        # Allow user to save normally
+        if user_message in ["/save", "/s"]:
+            save_session(session_name, messages)
+
         # Allow user to exit normally
         if user_message in ["/quit", "/q", "/exit", "/e"]:
-            save_session(session_name, messages)
+            save_session(session_name, messages)  # Auto save session
             exit()
 
+        # Handle user commands
         if user_message.startswith("/"):
-            command_response = handle_command(user_message)
+            command_response = command_factory(user_message)
             print(command_response)
             user_message = f"{user_message}\n{command_response}"
 
@@ -91,8 +122,10 @@ def main():
             encoding,
         )
 
+        print()
+
         # Use a prompt to identify GPT's output
-        print_bold("\nassistant")
+        print_bold("assistant")
 
         # Call the streaming API
         assistant_message = openai.stream_chat_completions(
@@ -106,7 +139,7 @@ def main():
         if assistant_message:
             assistant_content = assistant_message["content"]
             if assistant_content.startswith("/"):
-                command_response = handle_command(assistant_content)
+                command_response = command_factory(assistant_content)
                 print(command_response)
                 assistant_content = f"{assistant_content}\n{command_response}"
                 assistant_message = {"role": "assistant", "content": assistant_content}
@@ -119,7 +152,7 @@ def main():
                 encoding,
             )
 
-        print()  # output newline character
+        print("\n")  # output newline characters
 
         # Save the updated messages to the session file
         save_session(session_name, messages)
