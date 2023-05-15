406850964a5840e51b8e6b82a64beb4b8d470e6d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-15 02:44:48 -0400

    feat: Add prompt formatting outline
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

---
+++ pygptprompt/format.py
@@ -0,0 +1,14 @@
+import re
+
+from prompt_toolkit import print_formatted_text
+from prompt_toolkit.formatted_text import HTML
+
+
+def print_with_backticks_bold(text):
+    bold_text = re.sub(r"`(.*?)`", r"<b>\1</b>", text)
+    print_formatted_text(HTML(bold_text))
+
+
+if __name__ == "__main__":
+    # Usage:
+    print_with_backticks_bold("Hello, `world`!")
