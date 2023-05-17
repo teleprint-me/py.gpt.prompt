01f25bd49d2c343d50f4598a962ce7a86f03dfbd
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-17 18:45:00 -0400

    refactor: Fix and update directory listing
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- pygptprompt/command/list.py
+++ pygptprompt/command/list.py
@@ -1,47 +1,50 @@
 import os
 
-from pygptprompt.config import get_config
 from pygptprompt.policy import is_accessible
-
-__config__ = get_config()
 
 
 def get_file_info(file_path: str) -> str:
     is_dir = os.path.isdir(file_path)
+
     if is_dir:
         file_size = "-"
     else:
         file_size = os.path.getsize(file_path)
 
     type_info = "dir" if is_dir else "file"
+
     return f"{file_path:30} {type_info:10} {file_size}"
 
 
 def list_directory(command: str) -> str:
-    allowed_paths = [] if not __config__ else __config__["allowed_paths"]
-    denied_paths = [] if not __config__ else __config__["denied_paths"]
-
     # The command is the first argument.
     args = command.split()[1:]
 
     # The directory path is the second argument
-    directory = args[0].strip()
+    # Assume a directory path is given
+    # If no path is given, then assume local directory
+    try:
+        directory = args[0].strip()
+    except (IndexError,):
+        directory = "."
 
-    if not is_accessible(directory, allowed_paths, denied_paths):
+    if not is_accessible(directory):
         return "RoleError: Access denied! You shouldn't snoop in private places."
 
     if not os.path.isdir(directory):
         return f"Error: Directory '{directory}' not found."
 
     try:
+        header = f"{'File Path':30} {'Type Info':10} {'File Size in Bytes'}\n"
         files = [
             file
             for file in os.listdir(directory)
-            if is_accessible(os.path.join(directory, file), allowed_paths, denied_paths)
+            if is_accessible(os.path.join(directory, file))
         ]
         file_info_list = [
             get_file_info(os.path.join(directory, file)) for file in files
         ]
-        return "\n".join(file_info_list)
+        body = "\n".join(file_info_list)
+        return header + body
     except Exception as e:
         return f"Error listing directory {directory}: {str(e)}."
