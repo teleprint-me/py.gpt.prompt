d39026b124551fd51947be2999f299a15a25ea0d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-17 18:47:03 -0400

    refactor: Update help message to reflect command usage
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- pygptprompt/command/help.py
+++ pygptprompt/command/help.py
@@ -1,20 +1,21 @@
+# pygptprompt/command/help.py
 def display_help(command: str) -> str:
     help_text = """
     # Help
-
     ## Commands:
 
-    ### Web Pages
-    /robots <url>: Get robots.txt of URL.
-    /browse <url>: Fetch HTML from URL, convert to Markdown, and cache it locally. Returns local cache if content already exists.
+    **Sub-Process**
+    /: Execute shell command (restricted by configuration).
 
-    ### RSS Feeds
-    /rss <url>: Fetch and display full text articles from RSS feed.
+    **Web Pages**
+    /robots <url>: Fetch URL's robots.txt.
+    /browse <url>: Fetch, markdown-convert, and cache URL's HTML.
 
-    ### Filesystem (access restricted by configuration)
-    /ls <dir>: List files in local directory.
-    /read <file> [start_line] [end_line]: Read local file content from start_line to end_line (end_line is optional and line numbers start at 1).
-    /tree <directory> [--ignore dir1,dir2,...]: Display directory and file structure in a tree format. Optionally, ignore specified directories.
-    /git <params>: Access and execute local git commands.
+    **RSS Feeds**
+    /rss <url>: Display full-text articles from RSS feed.
+
+    **Filesystem** (access restricted by configuration)
+    /ls <dir>: List files in directory (defaults to current dir).
+    /read <file> [start] [end]: Display file content from start to end line (optional, defaults to full file).
     """
     return help_text.strip()
