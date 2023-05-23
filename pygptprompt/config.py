ffdacedb9bbec0a0b2e1087ead54aa9720cbb625
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-23 01:00:01 -0400

    refactor: Update Configuration class and tests
    - Refactor Configuration class to support exact path lookups and searching nested dicts.
    - Update get_value method to correctly use get_value_by_key with exact_path=True.
    - Update get_api_key method to use get_value to get the path to the environment file.
    - Add and update tests to cover changes and ensure Configuration class works as expected.

--- pygptprompt/config.py
+++ pygptprompt/config.py
@@ -11,9 +11,9 @@ from pygptprompt.singleton import Singleton
 class Configuration(Singleton):
     def __init__(self, filepath: Optional[str] = None):
         self.filepath = filepath if filepath else "config.json"
-        self.data = self.load_configuration()
+        self.data = self.load()
 
-    def load_configuration(self) -> dict[str, Any]:
+    def load(self) -> dict[str, Any]:
         with open(self.filepath, "r") as f:
             config = json.load(f)
 
@@ -26,21 +26,41 @@ class Configuration(Singleton):
         self,
         data: Union[list[Any], dict[str, Any]],
         key: str,
+        exact_path: bool = False,
     ) -> Any:
-        if isinstance(data, dict):
-            if key in data:
-                return data[key]
-
-            for val in data.values():
-                if isinstance(val, dict):
-                    result = self.get_value_by_key(val, key)
-
-                    if result is not None:
-                        return result
+        if exact_path:
+            keys = key.split(".")
+            for key in keys:
+                if isinstance(data, dict) and key in data:
+                    data = data[key]
+                else:
+                    return None
+            return data
+        else:
+            if isinstance(data, dict):
+                if key in data:
+                    return data[key]
+                for val in data.values():
+                    if isinstance(val, dict):
+                        result = self.get_value_by_key(val, key)
+                        if result is not None:
+                            return result
         return None
 
+    def get_value(self, key: str, default: Optional[Any] = None) -> Any:
+        keys = key.split(".")
+        data = self.data
+
+        for key in keys:
+            data = self.get_value_by_key(data, key, exact_path=True)
+
+            if data is None:
+                return default
+
+        return data
+
     def get_api_key(self) -> str:
-        env = self.get_value_by_key(self.data, "path.environment")
+        env = self.get_value("path.environment", ".env")
 
         if not dotenv.load_dotenv(env):
             raise ValueError("EnvironmentError: Failed to load `.env`")
@@ -51,15 +71,3 @@ class Configuration(Singleton):
             raise ValueError("EnvironmentError: Failed to load `OPENAI_API_KEY`")
 
         return api_key
-
-    def get_value(self, key: str, default: Optional[Any] = None) -> Any:
-        keys = key.split(".")
-        data = self.data
-
-        for key in keys:
-            data = self.get_value_by_key(data, key)
-
-            if data is None:
-                return default
-
-        return data
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -13,6 +13,14 @@ class TestConfiguration:
         assert isinstance(config, Singleton)
         assert isinstance(config, Configuration)
 
+    def test_attributes(self, config: Configuration):
+        assert hasattr(config, "filepath")
+        assert hasattr(config, "data")
+        assert hasattr(config, "load")
+        assert hasattr(config, "get_api_key")
+        assert hasattr(config, "get_value_by_key")
+        assert hasattr(config, "get_value")
+
     def test_filepath(self, config: Configuration):
         assert os.path.exists(config.filepath)
 
@@ -20,27 +28,42 @@ class TestConfiguration:
         assert isinstance(config.data, dict)
 
     def test_load_configuration(self, config: Configuration):
-        assert hasattr(config, "load_configuration")
         with open(config.filepath, "r") as f:
             expected_config = json.load(f)
         assert config.data == expected_config
 
     def test_get_value_by_key(self, config: Configuration):
-        assert hasattr(config, "get_value_by_key")
-        assert config.get_value_by_key(config.data, "chat_completions") is not None
+        assert bool(config.get_value_by_key(config.data, "chat_completions"))
+        assert isinstance(
+            config.get_value_by_key(
+                config.data,
+                "allowed_commands",
+            ),
+            list,
+        )
+        assert not isinstance(
+            config.get_value_by_key(
+                config.data,
+                "access.shell.allowed_commands",
+            ),
+            list,
+        )
+        assert isinstance(
+            config.get_value_by_key(
+                config.data,
+                "access.file.allowed_paths",
+                True,
+            ),
+            list,
+        )
 
     @pytest.mark.private
     def test_api_key(self, config: Configuration):
-        assert hasattr(config, "get_api_key")
         assert bool(config.get_api_key())
         assert isinstance(config.get_api_key(), str)
         assert config.get_api_key().startswith("sk-")
 
     def test_get_value(self, config: Configuration):
-        assert hasattr(config, "get_value")
-        assert config.get_value("chat_completions") is not None
+        assert bool(config.get_value("chat_completions"))
         assert config.get_value("non_existent_key", "default") == "default"
-        assert isinstance(
-            config.get_value_by_key(config.data, "allowed_commands"), list
-        )
         assert isinstance(config.get_value("access.shell.allowed_commands"), list)
