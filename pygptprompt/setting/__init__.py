3a3085699f0a616d63d77537f48b390085cf61a9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-23 23:46:58 -0400

    refactor: Port settings from QGPT to PyGPTPrompt
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

---
+++ pygptprompt/setting/__init__.py
@@ -0,0 +1,2 @@
+from pygptprompt.setting.config import GlobalConfiguration
+from pygptprompt.setting.json import force_read_json, read_json, write_json
---
+++ pygptprompt/setting/config.py
@@ -0,0 +1,52 @@
+# pygptprompt/setting/config.py
+import datetime
+import os
+import shutil
+from pathlib import Path
+from typing import Any, Optional
+
+import dotenv
+
+from pygptprompt.pattern import MappingTemplate, Singleton
+from pygptprompt.setting.json import read_json, write_json
+
+
+class GlobalConfiguration(Singleton, MappingTemplate):
+    def __init__(self, filepath: Optional[str] = None):
+        super()
+        self.filepath = Path(filepath if filepath else "config.json")
+        self._dataset = self.load()
+
+    def load(self) -> dict[str, Any]:
+        return read_json(self.filepath)
+
+    def save(self) -> None:
+        write_json(self.filepath, self._dataset)
+
+    def backup(self) -> None:
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = self.filepath.name.replace(".json", f"_{timestamp}.json")
+        backup_filename = self.filepath.parent / filename
+        shutil.copyfile(self.filepath, backup_filename)
+
+    def get_value(
+        self,
+        key: str,
+        default: Optional[Any] = None,
+    ) -> Any:
+        keys = key.split(".")
+        data = self.read_nested(*keys)
+        return data if data is not None else default
+
+    def get_api_key(self) -> str:
+        env = self.get_value("path.environment", ".env")
+
+        if not dotenv.load_dotenv(env):
+            raise ValueError("EnvironmentError: Failed to load `.env`")
+
+        api_key = os.getenv("OPENAI_API_KEY") or ""
+
+        if not api_key:
+            raise ValueError("EnvironmentError: Failed to load `OPENAI_API_KEY`")
+
+        return api_key
