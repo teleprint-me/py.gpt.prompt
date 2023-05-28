9ab102bbb687ac47b5d685de4cdc2f148d158db3
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-28 01:43:31 -0400

    patch: Fix handling policy access control for traversable paths
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- pygptprompt/command/process.py
+++ pygptprompt/command/process.py
@@ -27,12 +27,16 @@ class SubprocessRunner:
             if self.policy.is_traversable(arg):
                 if not self.policy.is_accessible(arg):
                     return f"AccessError: Access to file {arg} is not allowed."
-                else:
-                    return f"FileError: File {arg} does not exist or is not a file."
 
         # Run the command
         try:
-            result = subprocess.run(args, capture_output=True, text=True, check=True)
+            result = subprocess.run(
+                args,
+                capture_output=True,
+                text=True,
+                check=True,
+                shell=False,  # NOTE: Enabling this is potentially dangerous!
+            )
             return result.stdout
         except subprocess.CalledProcessError as e:
             return f"CommandError: Command '{command}' returned non-zero exit status {e.returncode}."
