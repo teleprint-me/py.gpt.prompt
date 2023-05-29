e12c27eeb8d779239a2a703914a43914daf9ccf9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-29 17:21:11 -0400

    refactor: RSSHandler class to handle fetching, parsing, and caching of RSS feeds
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- pygptprompt/command/feed.py
+++ pygptprompt/command/feed.py
@@ -26,12 +26,7 @@ class RSSHandler:
         # Try to read from cache
         cached_content = read_from_cache(cache_path)
         if cached_content is not None:
-            cached_content_size = self.queue_proxy.token.get_content_count(
-                cached_content
-            )
-            if cached_content_size > self.queue_proxy.token.base_limit:
-                return f"The content is too large to display. It has been saved to a file: {cache_path}"
-            return cached_content
+            return self.queue_proxy.handle_content_size(cached_content, cache_path)
 
         # Fetch the full text from the RSS feed
         feed_entries = self._fetch_full_text_from_rss_feed(feed_url)
@@ -45,17 +40,7 @@ class RSSHandler:
         # Always cache the result!
         write_to_cache(cache_path, feed_entries_content)
 
-        # Check if the content is too large
-        feed_entries_content_size = self.queue_proxy.token.get_content_count(
-            feed_entries_content
-        )
-
-        # the `base_limit` represents a percentage of the context window size
-        # the context window size is represented as the `upper_limit`
-        if feed_entries_content_size > self.queue_proxy.token.base_limit:
-            return f"The content is too large to display. It has been saved to a file: {cache_path}"
-
-        return feed_entries_content
+        return self.queue_proxy.handle_content_size(feed_entries_content, cache_path)
 
     def _parse_command(self, command: str) -> str:
         # Command is split into parts
@@ -83,6 +68,9 @@ class RSSHandler:
     def _fetch_rss_feed(self, feed_url: str) -> Union[str, FeedParserDict]:
         try:
             feed: FeedParserDict = feedparser.parse(feed_url)
+            # Check if the feed is empty
+            if not feed.entries:
+                return f"Error: The URL {feed_url} does not appear to be an RSS feed."
             return feed
         except Exception as e:
             return f"Error fetching RSS feed from {feed_url}: {str(e)}."
@@ -90,8 +78,11 @@ class RSSHandler:
     def _fetch_article_text(self, article_url: str) -> str:
         try:
             response = requests.get(article_url)
+            print(f"Response status code: {response.status_code}")  # Debugging line
             soup = BeautifulSoup(response.text, "html.parser")
-            return soup.get_text()
+            text = soup.get_text()
+            print(f"Extracted text: {text[:100].strip()}...")  # Debugging line
+            return text
         except Exception as e:
             return f"Error fetching article text from {article_url}: {str(e)}."
 
