eb6435049066d9e3394064243428cd3d1f843b6d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-29 14:15:58 -0400

    refactor: Improved command parsing, content fetching, caching, and error handling in RSSHandler
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- pygptprompt/command/feed.py
+++ pygptprompt/command/feed.py
@@ -1,41 +1,96 @@
 # pygptprompt/command/feed.py
+import os
 from typing import Union
+from urllib.parse import urlparse
 
 import feedparser
 import requests
 from bs4 import BeautifulSoup
 from feedparser.util import FeedParserDict
 
-from pygptprompt.session.policy import SessionPolicy
-from pygptprompt.setting.config import GlobalConfiguration
+from pygptprompt.command.webtools import read_from_cache, write_to_cache
+from pygptprompt.session.proxy import SessionQueueProxy
 
 
 class RSSHandler:
-    def __init__(self, config: GlobalConfiguration, policy: SessionPolicy):
-        self.config: GlobalConfiguration = config
-        self.policy: SessionPolicy = policy
+    def __init__(self, queue_proxy: SessionQueueProxy):
+        self.queue_proxy = queue_proxy
 
-    @staticmethod
-    def fetch_rss_feed(feed_url: str) -> Union[str, FeedParserDict]:
+    def execute(self, command: str) -> str:
+        # Parse the command and get the feed URL
+        feed_url = self._parse_command(command)
+
+        # Cache the content
+        cache_path = self._get_cache_path(feed_url)
+
+        # Try to read from cache
+        cached_content = read_from_cache(cache_path)
+        if cached_content is not None:
+            return cached_content
+
+        # Fetch the full text from the RSS feed
+        feed_entries = self._fetch_full_text_from_rss_feed(feed_url)
+
+        # If the feed_entries is a list, join them with "\n---\n"
+        if isinstance(feed_entries, list):
+            feed_entries_content = "\n---\n".join(feed_entries)
+        else:  # something went wrong...
+            return feed_entries  # return the error instead.
+
+        # Always cache the result!
+        write_to_cache(cache_path, feed_entries_content)
+
+        # Check if the content is too large
+        feed_entries_content_size = self.queue_proxy.token.get_content_count(
+            feed_entries_content
+        )
+
+        # the `base_limit` represents a percentage of the context window size
+        # the context window size is represented as the `upper_limit`
+        if feed_entries_content_size > self.queue_proxy.token.base_limit:
+            return f"The content is too large to display. It has been saved to a file: {cache_path}"
+
+        return feed_entries_content
+
+    def _parse_command(self, command: str) -> str:
+        # Command is split into parts
+        args = command.strip().split()
+        # URL is the second part (index 1)
+        url = args[1]
+
+        # Ensure URL starts with "http://" or "https://"
+        if not url.startswith(("http://", "https://")):
+            url = "https://" + url
+
+        return url
+
+    def _get_cache_path(self, feed_url: str) -> str:
+        # Get the storage path
+        storage_path = self.queue_proxy.config.get_value("path.storage", "storage")
+
+        # Create a path for the cache
+        parsed_url = urlparse(feed_url)
+        domain = parsed_url.netloc
+        cache_path = os.path.join(storage_path, "rss", domain, "feed.txt")
+
+        return cache_path
+
+    def _fetch_rss_feed(self, feed_url: str) -> Union[str, FeedParserDict]:
         try:
             feed: FeedParserDict = feedparser.parse(feed_url)
             return feed
         except Exception as e:
-            raise Exception(f"Error fetching RSS feed from {feed_url}: {str(e)}.")
+            return f"Error fetching RSS feed from {feed_url}: {str(e)}."
 
-    @staticmethod
-    def fetch_article_text(article_url: str) -> str:
+    def _fetch_article_text(self, article_url: str) -> str:
         try:
             response = requests.get(article_url)
             soup = BeautifulSoup(response.text, "html.parser")
             return soup.get_text()
         except Exception as e:
-            raise Exception(
-                f"Error fetching article text from {article_url}: {str(e)}."
-            )
+            return f"Error fetching article text from {article_url}: {str(e)}."
 
-    @staticmethod
-    def fetch_cleaned(article_text: str) -> str:
+    def _fetch_cleaned(self, article_text: str) -> str:
         # Split the content by newline
         lines: list[str] = article_text.split("\n")
         # Remove empty lines and append a newline to each line (except the last one)
@@ -49,29 +104,20 @@ class RSSHandler:
         # Join the cleaned lines
         return "".join(cleaned_lines).strip()
 
-    def fetch_full_text_from_rss_feed(self, feed_url: str) -> Union[str, list[str]]:
-        feed = self.fetch_rss_feed(feed_url)
+    def _fetch_full_text_from_rss_feed(self, feed_url: str) -> Union[str, list[str]]:
+        feed = self._fetch_rss_feed(feed_url)
 
-        if isinstance(feed, Exception):
-            return str(feed)
+        if isinstance(feed, str):
+            return feed
 
         full_text_articles = []
 
         for entry in feed.entries:
-            try:
-                article_text = self.fetch_article_text(entry.link)
-                cleaned_article_text = self.fetch_cleaned(article_text)
+            article_text = self._fetch_article_text(entry.link)
+            if isinstance(article_text, str):
+                cleaned_article_text = self._fetch_cleaned(article_text)
                 full_text_articles.append(cleaned_article_text)
-            except Exception as e:
-                full_text_articles.append(str(e))
+            else:
+                full_text_articles.append(article_text)
 
         return full_text_articles
-
-    def execute(self, command: str) -> str:
-        feed_url = command.replace("/rss", "").strip()
-        feed_entries = self.fetch_full_text_from_rss_feed(feed_url)
-
-        if isinstance(feed_entries, list):
-            return "\n---\n".join(feed_entries)
-        else:
-            return feed_entries
