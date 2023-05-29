246e54e7a770f6d5081ee08fb1176876d887380f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-29 00:04:56 -0400

    refactor: Update ReadFile class to use SessionQueueProxy and improve readability
    - Updated the ReadFile class to use the SessionQueueProxy for managing command context.
    - Refactored the execute method to improve readability and maintainability.
    - Improved error messages to be more informative and user-friendly.

--- pygptprompt/command/read.py
+++ pygptprompt/command/read.py
@@ -1,55 +1,60 @@
 # pygptprompt/command/read.py
 import os
+from typing import Optional
 
-from pygptprompt.session.policy import SessionPolicy
-from pygptprompt.setting.config import GlobalConfiguration
+from pygptprompt.session.proxy import SessionQueueProxy
 
 
 class ReadFile:
-    def __init__(self, config: GlobalConfiguration, policy: SessionPolicy):
-        self.config: GlobalConfiguration = config
-        self.policy: SessionPolicy = policy
+    def __init__(self, session_proxy: SessionQueueProxy):
+        self.session_proxy = session_proxy
+
+    def get_file_content(
+        self,
+        filepath: str,
+        start_line: int,
+        end_line: Optional[int] = None,
+    ) -> str:
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+            if end_line is None:
+                return "".join(lines[start_line:])
+            else:
+                return "".join(lines[start_line:end_line])
 
     def execute(self, command: str) -> str:
         # The command is the first argument.
         args = command.split()[1:]
 
         if not args:
-            return "Error: No file path specified."
+            return "ReadError: No file path specified."
 
         # The filepath is the second argument
         filepath = args[0]
 
-        if not self.policy.is_accessible(filepath):
-            return "RoleError: Access denied! You shouldn't snoop in private places."
+        if not self.session_proxy.policy.is_accessible(filepath):
+            return f"AccessError: Access to file {filepath} is not allowed."
 
         if not os.path.isfile(filepath):
-            return f"Error: Filepath '{filepath}' not found."
+            return f"ReadError: Filepath '{filepath}' not found."
 
         try:
             # Optional starting line number
             # NOTE: -1 to account for 0-indexing
             start_line = 0 if len(args) < 2 else int(args[1]) - 1
         except ValueError:
-            return "Error: <start_line> must be an integer."
+            return "ReadError: <start_line> must be an integer."
 
         try:
             # Optional ending line number
             end_line = None if len(args) < 3 else int(args[2])
         except ValueError:
-            return "Error: <end_line> must be an integer."
+            return "ReadError: <end_line> must be an integer."
 
         if end_line is not None and end_line <= start_line:
-            return (
-                "Error: Ending line number must be greater than starting line number."
-            )
+            return "ReadError: Ending line number must be greater than starting line number."
 
         try:
-            with open(filepath, "r") as f:
-                lines = f.readlines()
-                if end_line is None:
-                    return "".join(lines[start_line:])
-                else:
-                    return "".join(lines[start_line:end_line])
+            return self.get_file_content(filepath, start_line, end_line)
         except Exception as e:
-            return f"Error reading file {filepath}: {str(e)}."
+            return f"ReadError: Error reading file {filepath}: {str(e)}."
