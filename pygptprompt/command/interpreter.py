124fe726e0ce2af5763f1044bee5e853459a1c20
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-29 18:31:59 -0400

    refactor: Remove deprecated methods for handling large results in CommandInterpreter
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- pygptprompt/command/interpreter.py
+++ pygptprompt/command/interpreter.py
@@ -22,34 +22,20 @@ class CommandInterpreter:
         return line.strip().startswith(("`", "```", "````"))
 
     @staticmethod
-    def write_result_to_file(command_result: str) -> str:
-        # TODO: Implement write_result_to_file method
-        # Save the result to a file in storage/dirname/filename
-        # Filename could be a UUID.
-        file_information = "File information"
-        return file_information
-
-    @staticmethod
     def extract_code_blocks(message: str) -> list[str]:
         # TODO: Implement extract_code_blocks method
         # Extract file extensions from a block (e.g. ```py\nprint("Hello, World!")\n```)
         # `py` would be used for the file extension to cache the source
         return re.split(r"(```[^`]*```)", message)
 
-    def is_result_too_large(self, command_result: str) -> bool:
-        # TODO: Refine this check to ensure context window won't overflow
-        # This check should consider token count in addition to string length.
-        return len(command_result) > self.queue_proxy.token.upper_limit
-
     def execute_command(self, line: str) -> str:
         return command_factory(self.queue_proxy, line)
 
     def replace_line_with_result(self, line: str, command_result: str) -> str:
-        if self.is_result_too_large(command_result):
-            command_result = self.write_result_to_file(command_result)
         # NOTE: User needs to see result
-        print(f"\n```\n{command_result.strip()}\n```")  # NOTE: Leave this line here!
-        return f"{line}\n```\n{command_result.strip()}\n```"
+        padded_result = f"\n```\n{command_result.strip()}\n```"
+        print(padded_result)  # NOTE: Leave this line here!
+        return f"{line}\n{padded_result}\n"
 
     def interpret_message(self, message_content: str) -> str:
         lines: list[str] = message_content.split("\n")
