43f6bde2b6d8d90165021d550705999ef43567e0
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-01 00:58:34 -0400

    refactor: Rename cache_path parameter and improve content size message
    - Rename cache_path parameter to file_path in handle_content_size method
    - Improve content size message to provide clear outline for reading cached content
    - Improve code clarity and maintainability

--- pygptprompt/session/proxy.py
+++ pygptprompt/session/proxy.py
@@ -18,8 +18,8 @@ class SessionQueueProxy:
     def token(self):
         return self._session_queue.token
 
-    def handle_content_size(self, content: str, cache_path: str) -> str:
+    def handle_content_size(self, content: str, file_path: str) -> str:
         content_size = self.token.get_content_count(content)
         if content_size > self.token.base_limit:
-            return f"The content is too large to display. It has been saved to a file: {cache_path}"
+            return f"The content is too large to display.\nRead the cached content for the local file using the following outline.\nCommand outline: `/read {file_path} [start_line] [end_line]`"
         return content
