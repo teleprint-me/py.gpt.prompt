f6f138fa5179822d304ccbd5ed3614ec28838f9a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-01 00:38:59 -0400

    refactor: Refactor SubprocessRunner class to cache command output and improve modularity
    - Add _get_cache_path method to generate a unique file name for each command that is run
    - Import write_to_cache function from pygptprompt.command.webtools module
    - Modify execute method to write command output to a cache file and return content size
    - Import os and uuid4 modules for use in SubprocessRunner class
    - Improve code modularity and maintainability

--- pygptprompt/command/process.py
+++ pygptprompt/command/process.py
@@ -1,13 +1,28 @@
 # pygptprompt/command/process.py
+import os
 import shlex
 import subprocess
+from uuid import uuid4
 
+from pygptprompt.command.webtools import write_to_cache
 from pygptprompt.session.proxy import SessionQueueProxy
 
 
 class SubprocessRunner:
     def __init__(self, queue_proxy: SessionQueueProxy):
         self.queue_proxy = queue_proxy
+
+    def _get_cache_path(self, command: str) -> str:
+        # Get the storage path
+        storage_path = self.queue_proxy.config.get_value("path.storage", "storage")
+
+        # Generate a uniquely identifiable file name
+        file_name = f"{command}-{str(uuid4())[:6]}.log"
+
+        # Create a path for the cache
+        cache_path = os.path.join(storage_path, "shell", file_name)
+
+        return cache_path
 
     def execute(self, command: str) -> str:
         """Run a subprocess command and return its output."""
@@ -34,6 +49,8 @@ class SubprocessRunner:
                 check=True,
                 shell=False,  # NOTE: Enabling this is potentially dangerous!
             )
-            return result.stdout
+            cache_path = self._get_cache_path(args[0])
+            write_to_cache(cache_path, result.stdout)
+            return self.queue_proxy.handle_content_size(result.stdout, cache_path)
         except subprocess.CalledProcessError as e:
             return f"CommandError: Command '{command}' returned non-zero exit status {e.returncode}."
