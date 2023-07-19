0f653eead08a39fabe5105fa006c6231a98dbafb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-07-19 13:06:17 -0400

    refactor: Temporarily skip testing OpenCL until a valid solution is found

--- tests/unit/test_opencl.py
+++ tests/unit/test_opencl.py
@@ -2,6 +2,13 @@ import pyopencl
 import pytest
 
 
+# NOTE:
+# PyOpenCL and Installable Client Driver (ICD) must be compiled to specific hardware.
+# Using a virtual environment (e.g. Poetry) masks the system install because it's
+# job is to isolate packages from the system libraries. This creates a conflict
+# that will eventually need to be addressed. This is test is temporarily disabled
+# as a result of this issue.
+@pytest.mark.skip
 def test_opencl():
     # Get all available platforms
     platforms = pyopencl.get_platforms()
