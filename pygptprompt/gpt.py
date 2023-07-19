55023fa1c308295fadb91e9916027b0e68c3bcdd
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-07-19 14:23:39 -0400

    refactor: Add openai prototype interface to match llama API

--- pygptprompt/gpt.py
+++ pygptprompt/gpt.py
@@ -1,31 +1,101 @@
 import sys
+from typing import List
 
-import dotenv
-import openai
+import click
+from llama_cpp import ChatCompletionMessage
+from prompt_toolkit import prompt as input
 
-path = ".env"
+from pygptprompt import logging
+from pygptprompt.api.openai import OpenAIAPI
+from pygptprompt.config.manager import ConfigurationManager
 
-dotenv.load_dotenv(path)
 
-openai.api_key = dotenv.get_key(path, "OPENAI_API_KEY")
-
-chat_completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": "My name is ChatGPT. I am a helpful assistant.",
-        },
-        {
-            "role": "user",
-            "content": "What is your name?",
-        },
-    ],
-    stream=True,
+@click.command()
+@click.argument(
+    "config_path",
+    type=click.Path(exists=True),
+    default="config.json",
 )
+@click.option(
+    "--prompt",
+    type=click.STRING,
+    default=str(),
+    help="Prompt the model with a string.",
+)
+@click.option(
+    "--chat",
+    is_flag=True,
+    help="Enter a chat loop with the model.",
+)
+def main(config_path, prompt, chat):
+    if not (bool(prompt) ^ chat):
+        print(
+            "Use either --prompt or --chat, but not both.",
+            "See --help for more information.",
+        )
+        sys.exit(1)
 
-for packet in chat_completion:
-    if packet.choices[0].delta:
-        print(packet.choices[0].delta["content"], end="")
-        sys.stdout.flush()
-print()
+    config = ConfigurationManager(config_path)
+
+    openai = OpenAIAPI(api_key=config.get_api_key())
+
+    system_prompt = ChatCompletionMessage(
+        role=config.get_value("openai.system_prompt.role"),
+        content=config.get_value("openai.system_prompt.content"),
+    )
+
+    messages: List[ChatCompletionMessage] = [system_prompt]
+
+    try:
+        print(system_prompt.get("role"))
+        print(system_prompt.get("content"))
+        print()
+
+        if prompt:
+            user_prompt = ChatCompletionMessage(role="user", content=prompt)
+            messages.append(user_prompt)
+            print("assistant")
+            message: ChatCompletionMessage = openai.get_chat_completions(
+                messages=messages,
+                max_tokens=config.get_value("llama_cpp.chat_completions.max_tokens"),
+                temperature=config.get_value("llama_cpp.chat_completions.temperature"),
+                top_p=config.get_value("llama_cpp.chat_completions.top_p"),
+            )
+            messages.append(message)
+
+        elif chat:
+            while True:
+                try:
+                    print("user")
+                    text_input = input(
+                        "> ", multiline=True, wrap_lines=True, prompt_continuation=". "
+                    )
+                except (EOFError, KeyboardInterrupt):
+                    break
+                user_message = ChatCompletionMessage(role="user", content=text_input)
+                messages.append(user_message)
+
+                print()
+                print("assistant")
+                message: ChatCompletionMessage = openai.get_chat_completions(
+                    messages=messages,
+                    max_tokens=config.get_value(
+                        "llama_cpp.chat_completions.max_tokens"
+                    ),
+                    temperature=config.get_value(
+                        "llama_cpp.chat_completions.temperature"
+                    ),
+                    top_p=config.get_value("llama_cpp.chat_completions.top_p"),
+                )
+                print()
+                messages.append(message)
+
+        else:
+            print("Nothing to do.")
+    except Exception as e:
+        logging.error(f"Error generating response: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
