51461f53a7288a4db724e46e261de8bfa06edacf
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-07-19 11:36:51 -0400

    feat: Add function role to ChatCompletionMessage

---
+++ pygptprompt/session/message.py
@@ -0,0 +1,24 @@
+"""
+pygptprompt/session/message.py
+"""
+
+from typing import Literal, NotRequired
+
+from llama_cpp import ChatCompletionMessage
+
+
+class ExtendedChatCompletionMessage(ChatCompletionMessage):
+    """
+    Extended chat completion message with additional role options.
+
+    Inherits:
+        ChatCompletionMessage: Base chat completion message class.
+
+    Attributes:
+        role (Literal["assistant", "user", "system", "function"]): The role of the message.
+        content (str): The content of the message.
+        user (Optional[str]): The user associated with the message (optional).
+    """
+
+    role: Literal["assistant", "user", "system", "function"]
+    user: NotRequired[str]
