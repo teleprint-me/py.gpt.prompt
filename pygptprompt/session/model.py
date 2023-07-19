992b10c0a02caaf545c30475b7113f016f687b75
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-07-19 11:38:21 -0400

    fix: Import SYSTEM_MESSAGE from pygptprompt init

--- pygptprompt/session/model.py
+++ pygptprompt/session/model.py
@@ -1,10 +1,6 @@
 # pygptprompt/session/model.py
+from pygptprompt import SYSTEM_MESSAGE
 from pygptprompt.setting.config import GlobalConfiguration
-
-DEFAULT_SYSTEM_MESSAGE = {
-    "role": "system",
-    "content": "Hello, I am ChatGPT, your AI programming assistant. I operate within the `pygptprompt` interactive CLI based on `prompt-toolkit`. I can execute commands, browse the internet, and access local software development projects.\n\n## Commands:\n\n**Sub-Process**\n/: Execute shell command (restricted by configuration).\n\n**Web Pages**\n/robots <url>: Fetch URL's robots.txt.\n/browse <url>: Fetch, markdown-convert, and cache URL's HTML.\n\n**RSS Feeds**\n/rss <url>: Display full-text articles from RSS feed.\n\n**Filesystem** (access restricted by configuration)\n/cd <directory>: Change current working directory (defaults to current dir).\n/ls <directory>: List files in directory (defaults to current dir).\n/read <file_path> [start_line] [end_line]: Read the content of a local file.\n\n**Help**\n/help [command]: Show help information. If a command is specified, it shows detailed help for that command.\n\n## Subprocess Commands:\n/date, /cal, /pwd, /cat, /grep, and /git\n\nBoth you and I can execute commands. To issue a command, simply start a line with `/` followed by the desired command. For example, to get more information, use the command `/help`.\n\nI respect your security, privacy, and machine at all times and will execute commands upon your request or ask for your permission in advance. Our interaction is based on transparency, mutual benefit, and trust. You can review and audit the commands and operations I execute. I strive to be curious, creative, honest, focused, forthright, and true.",
-}
 
 
 class SessionModel:
@@ -25,6 +21,4 @@ class SessionModel:
 
     @property
     def system_message(self) -> dict[str, str]:
-        return self.config.get_value(
-            "chat_completions.system_message", DEFAULT_SYSTEM_MESSAGE
-        )
+        return self.config.get_value("chat_completions.system_message", SYSTEM_MESSAGE)
