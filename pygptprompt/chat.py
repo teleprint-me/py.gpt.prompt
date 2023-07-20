9c55d1c3edd275d08ecf96c21b9505c3bf8de64f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-07-19 22:30:53 -0400

    feat: Unify GPT and Llama chat scripts into a single script
    - Created a new script `chat.py` that combines the functionalities of `gpt.py` and `llama.py`.
    - Added a `--provider` option to the command line interface to allow users to specify the model they want to use.
    - Utilized `ConfigurationManager` for handling the configuration of the models.
    - Implemented error handling to catch exceptions during the generation of responses.
    - Preserved the ability to either provide a single prompt or enter a chat loop with the model.
    - Ensured clean exit points in the script where necessary for robustness.
    
    This commit unifies the separate GPT and Llama chat scripts into a single script, enhancing manageability and extensibility. It provides a flexible and robust foundation for the chat application.

---
+++ pygptprompt/chat.py
@@ -0,0 +1,103 @@
+import sys
+from typing import List
+
+import click
+from llama_cpp import ChatCompletionMessage
+from prompt_toolkit import prompt as input
+
+from pygptprompt import logging
+from pygptprompt.api.llama_cpp import LlamaCppAPI
+from pygptprompt.api.openai import OpenAIAPI
+from pygptprompt.config.manager import ConfigurationManager
+
+
+@click.command()
+@click.argument(
+    "config_path",
+    type=click.Path(exists=True),
+    default="config.json",
+)
+@click.option(
+    "--prompt",
+    type=click.STRING,
+    default=str(),
+    help="Prompt the model with a string.",
+)
+@click.option(
+    "--chat",
+    is_flag=True,
+    help="Enter a chat loop with the model.",
+)
+@click.option(
+    "--provider",
+    type=click.STRING,
+    default="llama_cpp",
+)
+def main(config_path, prompt, chat, provider):
+    if not (bool(prompt) ^ chat):
+        print(
+            "Use either --prompt or --chat, but not both.",
+            "See --help for more information.",
+        )
+        sys.exit(1)
+
+    config = ConfigurationManager(config_path)
+
+    if provider == config.get_value(provider, "openai"):
+        model = OpenAIAPI(config)
+    elif provider == config.get_value(provider, "llama_cpp"):
+        model = LlamaCppAPI(config)
+    else:
+        print(f"Unknown provider: {provider}")
+        sys.exit(1)
+
+    system_prompt = ChatCompletionMessage(
+        role=config.get_value("openai.system_prompt.role"),
+        content=config.get_value("openai.system_prompt.content"),
+    )
+
+    messages: List[ChatCompletionMessage] = [system_prompt]
+
+    try:
+        print(system_prompt.get("role"))
+        print(system_prompt.get("content"))
+        print()
+
+        if prompt:
+            user_prompt = ChatCompletionMessage(role="user", content=prompt)
+            messages.append(user_prompt)
+            print("assistant")
+            message: ChatCompletionMessage = model.get_chat_completions(
+                messages=messages,
+            )
+            messages.append(message)
+
+        elif chat:
+            while True:
+                try:
+                    print("user")
+                    text_input = input(
+                        "> ", multiline=True, wrap_lines=True, prompt_continuation=". "
+                    )
+                except (EOFError, KeyboardInterrupt):
+                    break
+                user_message = ChatCompletionMessage(role="user", content=text_input)
+                messages.append(user_message)
+
+                print()
+                print("assistant")
+                message: ChatCompletionMessage = model.get_chat_completions(
+                    messages=messages,
+                )
+                print()
+                messages.append(message)
+
+        else:
+            print("Nothing to do.")
+    except Exception as e:
+        logging.error(f"Error generating response: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
