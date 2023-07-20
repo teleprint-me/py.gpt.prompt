939a8b7ddff0d9083ff5a5cd61e501d5cdbceb9c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-07-19 21:22:34 -0400

    refactor: Streamline BaseAPI class interface for openai and llama.cpp

--- pygptprompt/api/base.py
+++ pygptprompt/api/base.py
@@ -1,17 +1,26 @@
 from abc import ABC, abstractmethod
+from typing import List, Union
+
+from llama_cpp import ChatCompletionMessage
+
+from pygptprompt.config.manager import ConfigurationManager
 
 
 class BaseAPI(ABC):
     @abstractmethod
-    def get_completions(self, **kwargs):
+    def __init__(self, config: ConfigurationManager):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_completions(self, prompt: str):
         # completions will eventually be deprecated
         # according to the OpenAI API documentation.
         raise NotImplementedError
 
     @abstractmethod
-    def get_chat_completions(self, **kwargs):
+    def get_chat_completions(self, messages: List[ChatCompletionMessage]):
         raise NotImplementedError
 
     @abstractmethod
-    def get_embeddings(self, **kwargs):
+    def get_embeddings(self, input: Union[str, List[str]]):
         raise NotImplementedError
