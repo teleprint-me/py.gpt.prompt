c9d1cdeb446aa3c44964ce7be2eb416807e899b2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-08-02 14:49:19 -0400

    refactor: Fix and update chat model factory tests
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- tests/unit/test_chat_model_factory.py
+++ tests/unit/test_chat_model_factory.py
@@ -3,19 +3,19 @@ tests/unit/test_chat_model_factory.py
 """
 import pytest
 
-from pygptprompt.api.factory import ChatModelFactory
-from pygptprompt.api.llama_cpp import LlamaCppAPI
-from pygptprompt.api.openai import OpenAIAPI
+from pygptprompt.model.factory import ChatModelFactory
+from pygptprompt.model.llama_cpp import LlamaCppModel
+from pygptprompt.model.openai import OpenAIModel
 
 
 class TestChatModelFactory:
     def test_create_openai_model(self, chat_model_factory: ChatModelFactory):
         model = chat_model_factory.create_model("openai")
-        assert isinstance(model, OpenAIAPI)
+        assert isinstance(model, OpenAIModel)
 
     def test_create_llama_cpp_model(self, chat_model_factory: ChatModelFactory):
         model = chat_model_factory.create_model("llama_cpp")
-        assert isinstance(model, LlamaCppAPI)
+        assert isinstance(model, LlamaCppModel)
 
     def test_unknown_provider(self, chat_model_factory: ChatModelFactory):
         with pytest.raises(ValueError):
