2419da1646301e5317e5c95d28ddc7667e66619e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-08-04 20:27:46 -0400

    refactor: ChatMessageHandler class and add docstrings
    - Renamed SessionToken to ChatMessageHandler for better clarity.
    - Added docstrings to the class constructor and properties.
    - Documented the purpose and arguments of each method.
    - Updated UML diagram to reflect the changes.

--- pygptprompt/session/token.py
+++ pygptprompt/session/token.py
@@ -1,69 +1,130 @@
-# pygptprompt/session/token.py
-import tiktoken
-from tiktoken import Encoding
+"""
+pygptprompt/session/token.py
+"""
+from typing import List, Literal
 
-from pygptprompt.setting.config import GlobalConfiguration
-
-# Define a dictionary for token limits
-TOKEN_LIMITS = {
-    "gpt-3.5-turbo": 4096,
-    "gpt-4": 8192,
-    "default": 2048,
-}
-TOKEN_OFFSET = 512
+from pygptprompt.config.manager import ConfigurationManager
+from pygptprompt.pattern.model import ChatModel
+from pygptprompt.pattern.types import ChatModelChatCompletion
 
 
-class SessionToken:
-    def __init__(self, config: GlobalConfiguration):
-        self.model: str = config.get_value("chat_completions.model", "gpt-3.5-turbo")
-        self.max_tokens: int = config.get_value("chat_completions.max_tokens", 1024)
-        self.base_limit_percentage: float = config.get_value(
-            "chat_completions.base_limit_percentage", 0.1
-        )
-        self.encoding: Encoding = tiktoken.encoding_for_model(self.model)
+class ChatMessageHandler:
+    """
+    A helper class for managing individual chat messages within a chat session.
+
+    Args:
+        provider (str): The provider or source of the chat session.
+        config (ConfigurationManager): The configuration manager for chat settings.
+        model (ChatModel): The chat model used for processing messages.
+    """
+
+    def __init__(
+        self,
+        provider: str,
+        config: ConfigurationManager,
+        model: ChatModel,
+    ):
+        """
+        Initializes the ChatMessageHandler.
+
+        Args:
+            provider (str): The provider or source of the chat session.
+            config (ConfigurationManager): The configuration manager for chat settings.
+            model (ChatModel): The chat model used for processing messages.
+        """
+        self._provider = provider
+        self._config = config
+        self._model = model
+
+    @property
+    def reserve(self) -> float:
+        """A floating-point value between 0 and 1 that represents a percentage of the maximum sequence length for the given model."""
+        return self._config.get_value(f"{self._provider}.context.reserve")
+
+    @property
+    def offset(self) -> int:
+        """The number of tokens to offset within a given sequence."""
+        return self._config.get_value(f"{self._provider}.context.offset")
+
+    @property
+    def max_length(self) -> int:
+        """An integer value representing the maximum sequence length for the given model."""
+        return self._config.get_value(f"{self._provider}.context.length")
+
+    @property
+    def max_tokens(self) -> int:
+        """An integer value representing the maximum sequence length the model is allowed to generate."""
+        return self._config.get_value(f"{self._provider}.chat_completions.max_tokens")
 
     @property
     def upper_limit(self) -> int:
-        # Get the token limit for the model from the dictionary, or use the default limit
-        token_limit: int = TOKEN_LIMITS.get(self.model, TOKEN_LIMITS["default"])
-        upper_limit: int = token_limit - self.max_tokens
-        return upper_limit
+        """Get the token limit for the model from the dictionary, or use the default limit."""
+        return self.max_length - self.max_tokens
 
     @property
     def base_limit(self) -> int:
-        # Calculate the base limit as a percentage of the upper limit
-        base_limit: int = int(self.upper_limit * self.base_limit_percentage)
-        return base_limit
+        """Calculate the base limit as a percentage of the upper limit."""
+        return int(self.upper_limit * self.reserve)
 
-    def get_content_count(self, content: str) -> int:
-        return len(self.encoding.encode(content))
+    def get_sequence_length(self, text: str) -> int:
+        """Count the number of tokens within the given text sequence."""
+        return len(self._model.get_encoding(text=text))
 
-    def get_message_count(self, message: dict[str, str]) -> int:
-        """Returns the number of tokens in a text string."""
-        if "content" in message:
-            content: str = message["content"]
-        else:
-            content: str = ""
+    def get_message_length(self, message: ChatModelChatCompletion) -> int:
+        """
+        Returns the number of tokens in a given message.
 
-        return self.get_content_count(content)
+        Args:
+            message (ChatModelChatCompletion): The message to process.
 
-    def get_total_message_count(self, messages: list[dict[str, str]]) -> int:
-        """Returns the number of tokens in a text string."""
+        Returns:
+            int: The number of tokens in the message.
+        """
+        sequence: str = ""
+
+        for key, value in message.items():
+            if value is not None:
+                sequence += " " + key + " " + value
+
+        return self.get_sequence_length(sequence.strip())
+
+    def get_total_message_length(
+        self,
+        messages: List[ChatModelChatCompletion],
+    ) -> int:
+        """
+        Returns the total number of tokens in a list of chat messages.
+
+        Args:
+            messages (List[ChatModelChatCompletion]): The list of messages.
+
+        Returns:
+            int: The total number of tokens in the list of messages.
+        """
         total_tokens: int = 0
 
         for message in messages:
-            total_tokens += self.get_message_count(message)
+            total_tokens += self.get_message_length(message)
 
         return total_tokens
 
     def is_overflow(
         self,
-        new_message: dict[str, str],
-        messages: list[dict[str, str]],
-        token_offset: int = TOKEN_OFFSET,
+        new_message: ChatModelChatCompletion,
+        messages: List[ChatModelChatCompletion],
     ) -> bool:
-        new_message_token_count = self.get_message_count(new_message)
-        messages_total_token_count = self.get_total_message_count(messages)
+        """
+        Check if adding a new message will cause the sequence to overflow.
+
+        Args:
+            new_message (ChatModelChatCompletion): The new message to be added.
+            messages (List[ChatModelChatCompletion]): The existing list of messages.
+
+        Returns:
+            bool: True if the sequence will overflow, False otherwise.
+        """
+        new_message_token_count = self.get_message_length(new_message)
+        messages_total_token_count = self.get_total_message_length(messages)
         token_count = new_message_token_count + messages_total_token_count
-        total_token_count = token_offset + token_count
+        total_token_count = self.offset + token_count
         return total_token_count >= self.upper_limit
