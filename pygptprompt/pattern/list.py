1895cb5174260eef610a24bb781818a073d369b1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-08-13 20:34:47 -0400

    refactor: Apply JSON Template inheritance, add pop method, fix type aliases

--- pygptprompt/pattern/list.py
+++ pygptprompt/pattern/list.py
@@ -2,22 +2,29 @@
 pygptprompt/pattern/list.py
 """
 from copy import deepcopy
-from typing import Any, Dict, List, Optional
+from typing import Optional
+
+from pygptprompt.pattern.json import JSONList, JSONMap, JSONTemplate
 
 
-class ListTemplate:
+class ListTemplate(JSONTemplate):
     """
-    A template class for managing a list of dictionaries.
+    A template class for managing a list of dictionaries in JSON files.
 
-    Args:
-        initial_data (Optional[List[Dict[str, Any]]]): Optional initial data to populate the list.
-
-    Attributes:
-        _data (List[Dict[str, Any]]): The underlying data structure for storing dictionaries.
+    Properties:
+        _file_path (Path): The file path of the JSON file.
+        _data (JSONList): The underlying data structure for storing dictionaries. Uses initial_data if not None, otherwise loads JSON data from file_path. Raises an error if no data is given.
     """
 
-    def __init__(self, initial_data: Optional[List[Dict[str, Any]]] = None):
-        self._data = initial_data if initial_data is not None else []
+    def __init__(self, file_path: str, initial_data: Optional[JSONList] = None):
+        """
+        Initialize a ListTemplate instance.
+
+        Args:
+            file_path (str): The path to the JSON file that stores the list.
+            initial_data (Optional[JSONList]): Optional initial data to populate the list.
+        """
+        super(ListTemplate, self).__init__(file_path, initial_data)
 
     @property
     def length(self) -> int:
@@ -25,25 +32,29 @@ class ListTemplate:
         return len(self._data)
 
     @property
-    def data(self) -> Optional[List[Dict[str, Any]]]:
+    def data(self) -> Optional[JSONList]:
         """Return a copy of the internal data list or None if empty."""
         return deepcopy(self._data) if self._data else None
 
-    def observe(self, data: List[Dict[str, Any]]) -> None:
-        """Update the underlying data structure with new data."""
-        self._data = data
+    def append(self, item: JSONMap) -> None:
+        """
+        Append a dictionary to the internal data list.
 
-    def append(self, item: Dict[str, Any]) -> None:
-        """Append a dictionary to the internal data list."""
+        Parameters:
+            item (JSONMap): The dictionary to append to the internal list.
+
+        Returns:
+            None
+        """
         self._data.append(item)
 
-    def insert(self, index: int, item: Dict[str, Any]) -> bool:
+    def insert(self, index: int, item: JSONMap) -> bool:
         """
         Insert a dictionary at a specific index.
 
         Parameters:
             index (int): The index at which to insert the dictionary.
-            item (Dict[str, Any]): The dictionary to insert.
+            item (JSONMap): The dictionary to insert.
 
         Returns:
             bool: True if successful, False otherwise.
@@ -53,7 +64,7 @@ class ListTemplate:
         self._data.insert(index, item)
         return True
 
-    def get(self, index: int) -> Optional[Dict[str, Any]]:
+    def get(self, index: int) -> Optional[JSONMap]:
         """
         Get a dictionary at a specific index.
 
@@ -61,17 +72,17 @@ class ListTemplate:
             index (int): The index of the dictionary to retrieve.
 
         Returns:
-            Optional[Dict[str, Any]]: The dictionary at the specified index or None if index is out of range.
+            Optional[JSONMap]: The dictionary at the specified index or None if index is out of range.
         """
         return self._data[index] if 0 <= index < len(self._data) else None
 
-    def update(self, index: int, item: Dict[str, Any]) -> bool:
+    def update(self, index: int, item: JSONMap) -> bool:
         """
         Update a dictionary at a specific index.
 
         Parameters:
             index (int): The index of the dictionary to update.
-            item (Dict[str, Any]): The dictionary with updated values.
+            item (JSONMap): The dictionary with updated values.
 
         Returns:
             bool: True if successful, False otherwise.
@@ -95,6 +106,20 @@ class ListTemplate:
             return False
         del self._data[index]
         return True
+
+    def pop(self, index: int) -> Optional[JSONMap]:
+        """
+        Remove and return a dictionary at a specific index.
+
+        Parameters:
+            index (int): The index of the dictionary to pop.
+
+        Returns:
+            JSONMap: A dictionary if successful, None otherwise.
+        """
+        if index < 0 or index >= len(self._data):
+            return None
+        return self._data.pop(index)
 
     def clear(self) -> None:
         """Clear the internal data list."""
