5c94527452bf96ec19bc3f5483fa4b504195b2b5
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-16 16:46:26 -0400

    chore: Update version to 0.0.30, add runtime download notes, and remove dead code
    - Updated package version to 0.0.30 in pyproject.toml.
    - Added runtime download notes to __init__.py.
    - Removed redundant and dead code.
    
    This commit increments the package version, includes notes regarding runtime model downloads, and removes redundant and dead code for improved codebase cleanliness.

--- pygptprompt/__init__.py
+++ pygptprompt/__init__.py
@@ -4,59 +4,42 @@
 
 pygptprompt/__init__.py
 
-Initialization code and configuration settings for the PyGPTPrompt package.
+NOTE:
 
-NOTE: Llama models usually have a maximum token limit of 2048.
+Models can be manually downloaded before runtime.
+
+Models can be optionally downloaded at runtime.
+
 The context window size may vary from model to model and can
 be easily adjusted as required.
 
 NOTE: IMPORTANT: MODEL_REPOSITORY
-Models are downloaded at runtime.
-The label convention is <username>/<repository>
-where <username>/<repository> represents the url endpoint
-where `hf_hub_download` writes [source] to [destination].
+
+Model reference:
+
+  <username>/<repository>
+
+  - where <username>/<repository> represents the url endpoint
+
+  - where `hf_hub_download` writes [source] to [destination].
+
 For example:
   ~/.cache/huggingface/hub/models--TheBloke--orca_mini_7B-GGML
 
 NOTE: IMPORTANT: MODEL_FILENAME
-Models are downloaded at runtime.
-The identifier convention is
-<model-id>.<n-params>.<ggml-version>.<quant-type>.<extension>
-where <model-id> represents the specific model identifier.
+
+Identifier reference:
+
+  <model-id>.<n-params>.<ggml-version>.<quant-type>.<extension>
+
+  - where <model-id> represents the specific model identifier.
+
 For example:
   orca-mini-7b.ggmlv3.q2_K.bin
-The filename follows the convention for the downloaded model files.
 """
-
-import logging
 import os
-from pathlib import Path
-from typing import List, Union
 
-# from pygptprompt.processor.image import ImageProcessor
-# from pygptprompt.processor.pdf import PDFProcessor
-
-__version__ = "0.0.20"
-
-# Set logging configuration
-# NOTE: Can be overridden on a script-by-script basis
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s",
-    level=logging.INFO,
-)
-
-# Get the user's home path
-PATH_HOME: Union[str, Path] = Path.home()
-# Set the default cache path
-PATH_CACHE: Union[str, Path] = Path(PATH_HOME, ".cache", "pygptprompt")
-# Set the default config path
-PATH_CONFIG: Union[str, Path] = Path(PATH_HOME, ".config", "pygptprompt")
-# Set the default local path
-PATH_LOCAL: Union[str, Path] = Path(PATH_HOME, ".local", "share", "pygptprompt")
-# Set the default path for storing the source documents
-PATH_SOURCE: Union[str, Path] = Path(PATH_CACHE, "source")
-# Set the default path for storing the database
-PATH_DATABASE: Union[str, Path] = Path(PATH_CACHE, "database")
+__version__ = "0.0.30"
 
 # The number of CPU threads for ingestion
 # Use os.sched_getaffinity(0) to get the set of threads
@@ -69,35 +52,13 @@ try:
 except AttributeError:
     CPU_COUNT: int = os.cpu_count() or 2
 
-# The default system message for the employed model
-SYSTEM_MESSAGE = {
-    "role": "system",
-    "content": "My name is GPT. I am a helpful programming assistant.",
-}
-
-# A List of PyTorch supported device types
-TORCH_DEVICE_TYPES: List[str] = [
-    "cpu",
-    "cuda",
-    "ipu",
-    "xpu",
-    "mkldnn",
-    "opengl",
-    "opencl",
-    "ideep",
-    "hip",
-    "ve",
-    "fpga",
-    "ort",
-    "xla",
-    "lazy",
-    "vulkan",
-    "mps",
-    "meta",
-    "hpu",
-    "mtia",
-]
-
+# NOTE:
+# This is here for future reference.
+# This will be removed once obsoleted.
+#
+# from pygptprompt.processor.image import ImageProcessor
+# from pygptprompt.processor.pdf import PDFProcessor
+#
 # # A mapping of MIME types to langchain loader classes
 # MAP_MIME_TYPES: tuple[tuple[str, type[object]], ...] = (
 #     # ("text/plain", TextProcessor),
