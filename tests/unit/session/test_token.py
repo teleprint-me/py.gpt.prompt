541fd061b4a1227fb237e90844aa71846250339e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-17 12:58:26 -0400

    test_token: Enhanced and clarified test names for ChatSessionTokenManager methods
    - Updated test names to better reflect the enhanced and more explicit method names in ChatSessionTokenManager.
    - Added comments to provide context for each test method.
    - Ensured the test suite maintains its comprehensiveness and structure.
    
    This commit aligns the test suite with the recent improvements in method naming for better clarity and understanding.

--- tests/unit/session/test_token.py
+++ tests/unit/session/test_token.py
@@ -19,57 +19,80 @@ class TestChatSessionTokenManager:
         assert isinstance(token_manager._config, ConfigurationManager)
         assert isinstance(token_manager._model, ChatModel)
 
-    def test_reserve_range(self, token_manager: ChatSessionTokenManager):
+    def test_calculate_reserve_in_range(self, token_manager: ChatSessionTokenManager):
+        """
+        Test that the calculated reserve is within the valid range [0, 1].
+        """
         assert 0 <= token_manager.reserve <= 1
 
-    def test_offset(self, token_manager: ChatSessionTokenManager):
+    def test_calculate_offset(self, token_manager: ChatSessionTokenManager):
+        """
+        Test the calculation of token offset within a sequence.
+        """
         assert isinstance(token_manager.offset, int)
 
-    def test_max_length(self, token_manager: ChatSessionTokenManager):
+    def test_calculate_max_length(self, token_manager: ChatSessionTokenManager):
+        """
+        Test the calculation of the maximum sequence length for the model.
+        """
         assert isinstance(token_manager.max_length, int)
 
-    def test_max_tokens(self, token_manager: ChatSessionTokenManager):
+    def test_calculate_max_tokens(self, token_manager: ChatSessionTokenManager):
+        """
+        Test the calculation of the maximum sequence length allowed for model output.
+        """
         assert isinstance(token_manager.max_tokens, int)
 
-    def test_upper_bound(self, token_manager: ChatSessionTokenManager):
+    def test_calculate_upper_bound(self, token_manager: ChatSessionTokenManager):
+        """
+        Test the calculation of the upper bound for the model's output.
+        """
         assert isinstance(token_manager.upper_bound, int)
 
     @pytest.mark.parametrize("reserve", [0.1, 0.5, 0.9])
-    def test_reserve_setting(
-        self,
-        token_manager: ChatSessionTokenManager,
-        reserve: float,
+    def test_set_reserve_setting(
+        self, token_manager: ChatSessionTokenManager, reserve: float
     ):
+        """
+        Test setting and calculating the reserve value.
+        """
         token_manager._config.set_value("llama_cpp.context.reserve", reserve)
         assert isinstance(token_manager.reserve, float)
 
-    def test_get_sequence_length(self, token_manager: ChatSessionTokenManager):
+    def test_calculate_sequence_length(self, token_manager: ChatSessionTokenManager):
+        """
+        Test the calculation of token sequence length from a text input.
+        """
         text = "This is a test text."
-        length = token_manager.get_sequence_length(text)
+        length = token_manager.calculate_text_sequence_length(text)
         assert isinstance(length, int)
 
-    def test_get_message_length_with_fixture(
-        self,
-        token_manager: ChatSessionTokenManager,
-        message: ChatModelResponse,
+    def test_calculate_message_length_with_fixture(
+        self, token_manager: ChatSessionTokenManager, message: ChatModelResponse
     ):
-        length = token_manager.get_message_length(message)
+        """
+        Test the calculation of token message length using a fixture.
+        """
+        length = token_manager.calculate_chat_message_length(message)
         assert isinstance(length, int)
 
-    def test_get_total_message_length_with_fixture(
-        self,
-        token_manager: ChatSessionTokenManager,
-        messages: List[ChatModelResponse],
+    def test_calculate_total_sequence_length_with_fixture(
+        self, token_manager: ChatSessionTokenManager, messages: List[ChatModelResponse]
     ):
-        total_length = token_manager.get_total_message_length(messages)
+        """
+        Test the calculation of total token sequence length using a fixture.
+        """
+        total_length = token_manager.calculate_chat_sequence_length(messages)
         assert isinstance(total_length, int)
 
-    def test_is_overflow_with_fixture(
+    def test_causes_sequence_overflow_with_fixture(
         self,
         token_manager: ChatSessionTokenManager,
         message: ChatModelResponse,
         messages: List[ChatModelResponse],
     ):
-        overflow = token_manager.is_overflow(message, messages)
-        assert isinstance(overflow, bool)
+        """
+        Test if adding a new message causes a sequence overflow using a fixture.
+        """
+        overflow = token_manager.causes_chat_sequence_overflow(message, messages)
         assert isinstance(overflow, bool)
