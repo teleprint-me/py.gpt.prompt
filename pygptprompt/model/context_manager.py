5b79d717d7d6acc6bdc8fb4cd4ab6f93c154b13d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-19 19:02:54 -0400

    doc: Add docstrings to ContextWindowManager class
    - Added docstrings to the ContextWindowManager class following PEP-257 guidelines.
    - Documented the class attributes, constructor, and methods.
    - Clarified the purpose and functionality of the class in the docstrings.

--- pygptprompt/model/context_manager.py
+++ pygptprompt/model/context_manager.py
@@ -14,6 +14,30 @@ from pygptprompt.pattern.model import ChatModel, ChatModelResponse
 # This change may require restructuring the codebase and could have a ripple effect.
 # Revisit this in a future sprint or version update when there's more time for planning and testing.
 class ContextWindowManager(TranscriptManager):
+    """
+    A class for managing chat model context windows.
+
+    This class extends TranscriptManager to provide functionality for managing chat model context windows.
+
+    Args:
+        file_path (str): The file path for storing the chat context.
+        provider (str): The provider of the chat model.
+        config (ConfigurationManager): The configuration manager for the application.
+        chat_model (ChatModel): The chat model used for generating responses.
+        vector_store (Optional[ChromaVectorStore]): An optional vector store for embedding messages.
+        embed (bool, optional): A flag indicating whether to embed messages in the vector store.
+
+    Attributes:
+        token_manager (ContextWindowTokenManager): The token manager for handling chat tokens.
+        vector_store (Optional[ChromaVectorStore]): An optional vector store for embedding messages.
+        embed (bool): A flag indicating whether to embed messages in the vector store.
+
+    Methods:
+        dequeue(): Dequeues the oldest message from the context window.
+        _append_single_message(message: ChatModelResponse): Appends a single message to the context window.
+
+    """
+
     def __init__(
         self,
         file_path: str,
@@ -33,18 +57,35 @@ class ContextWindowManager(TranscriptManager):
         self.embed = embed
 
     def dequeue(self) -> None:
-        # NOTE:
-        # The first element is always the system message.
-        # We always skip the system message in order to preserve the models directive.
-        # Logic to dequeue, possibly embed into vectors and store in vector store.
+        """
+        Dequeues the oldest message from the context window.
+
+        The first element is always the system message. This method skips the system message
+        in order to preserve the model's directive. It also provides logic for embedding
+        messages into vectors and storing them in the vector store if embedding is enabled.
+
+        Returns:
+            None
+        """
         dequeued_message = self.sequence.pop(1)
-        # NOTE: Embedding messages is optional and is set by user at runtime.
+        # Embedding messages is optional and is set by the user at runtime.
         if self.vector_store is not None and self.embed:
             self.vector_store.add_message_to_collection(dequeued_message)
 
     def _append_single_message(self, message: ChatModelResponse) -> None:
-        # NOTE: We have to override this method from the parent class
-        # Logic to append a single message, might want to check token size here
+        """
+        Appends a single message to the context window.
+
+        This method appends a single message to the context window. It checks the token size
+        to determine if the message causes a chat sequence overflow and dequeues the oldest
+        message if necessary.
+
+        Args:
+            message (ChatModelResponse): The message to append to the context window.
+
+        Returns:
+            None
+        """
         if self.token_manager.causes_chat_sequence_overflow(message, self.sequence):
             self.dequeue()
         self.sequence.append(message)
