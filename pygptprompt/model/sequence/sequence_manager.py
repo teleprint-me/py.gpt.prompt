c5684043dbe1c6b4af0db2fee9253599448ba074
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-24 14:09:37 -0400

    refactor: Improve encapsulation and optimize system message handling
    - Encapsulated the internal sequence and token manager properties
    - Optimized system message handling to have constant time complexity
    - Added documentation note for the sequence property
    
    This commit improves the encapsulation of internal properties and optimizes the handling of system messages in the SequenceManager class. It also adds a documentation note to clarify the expected structure of the sequence.

--- pygptprompt/model/sequence/sequence_manager.py
+++ pygptprompt/model/sequence/sequence_manager.py
@@ -62,50 +62,60 @@ class SequenceManager(Protocol):
             level="DEBUG",
         )
 
-        self.list_template = ListTemplate(file_path=file_path, logger=self.logger)
+        self._sequence = []
 
-        self.token_manager = TokenManager(
+        self._token_manager = TokenManager(
             provider=provider, config=config, chat_model=chat_model
         )
 
-        self.sequence = []
+        self._list_template = ListTemplate(file_path=file_path, logger=self.logger)
 
     def __len__(self) -> int:
         """Get the length of the sequence."""
-        return len(self.sequence)
+        return len(self._sequence)
 
     def __getitem__(self, index) -> ChatModelResponse:
         """Get a ChatModelResponse at the specified index."""
-        return self.sequence[index]
+        return self._sequence[index]
 
     def __setitem__(self, index: int, value: ChatModelResponse):
         """Set a ChatModelResponse at the specified index."""
-        self.sequence[index] = value
+        self._sequence[index] = value
 
     def __delitem__(self, index: int):
         """Delete a ChatModelResponse at the specified index."""
-        del self.sequence[index]
+        del self._sequence[index]
 
     def __iter__(self) -> Iterator[ChatModelResponse]:
         """Get an iterator for the sequence."""
-        return iter(self.sequence)
+        return iter(self._sequence)
 
     def __contains__(self, item: ChatModelResponse):
         """Check if a ChatModelResponse is in the sequence."""
-        return item in self.sequence
+        return item in self._sequence
 
     @property
-    def system_message(self) -> ChatModelResponse:
+    def sequence(self) -> List[ChatModelResponse]:
         """
-        Get the system message at the beginning of the sequence.
+        Get the sequence of ChatModelResponse objects.
 
         Returns:
-            ChatModelResponse: The system message.
+            List[ChatModelResponse]: The sequence of chat responses.
+
+        NOTE:
+            A sequence is expected to only ever contain a single system message, and the system message should be the first element. Any other form is considered undefined behavior.
         """
-        try:
-            return self.sequence[0]
-        except IndexError:
-            return ChatModelResponse(role="", content="")
+        return self._sequence
+
+    @property
+    def token_manager(self) -> TokenManager:
+        """
+        Get the token manager for handling chat tokens.
+
+        Returns:
+            TokenManager: The token manager instance.
+        """
+        return self._token_manager
 
     @property
     def token_count(self) -> int:
@@ -115,7 +125,41 @@ class SequenceManager(Protocol):
         Returns:
             int: The total number of tokens.
         """
-        return self.token_manager.calculate_chat_sequence_length(self.sequence)
+        return self._token_manager.calculate_chat_sequence_length(self._sequence)
+
+    @property
+    def system_message(self) -> ChatModelResponse:
+        """
+        Get the system message at the beginning of the sequence.
+
+        Returns:
+            ChatModelResponse: The system message, or an empty message if no system prompt is found.
+        """
+        if self._sequence and self._sequence[0]["role"] == "system":
+            return self._sequence[0]
+
+        # If no system prompt is found, return an empty message
+        return ChatModelResponse(role="", content="")
+
+    @system_message.setter
+    def system_message(self, value: ChatModelResponse) -> None:
+        """
+        Set or modify the system message.
+
+        Args:
+            value (ChatModelResponse): The new system message.
+        """
+        if len(self._sequence) > 0:
+            # Check the role of the first message
+            if self._sequence[0]["role"] == "system":
+                # Replace the existing system message
+                self._sequence[0] = value
+            else:
+                # Insert a new system message at the beginning
+                self._sequence.insert(0, value)
+        else:
+            # If the sequence is empty, add the system message
+            self._sequence.append(value)
 
     def load_to_chat_completions(self) -> bool:
         """
@@ -124,9 +168,9 @@ class SequenceManager(Protocol):
         Returns:
             bool: True if loading was successful, False on error.
         """
-        if self.list_template.load_json():
-            self.sequence = [
-                ChatModelResponse(**message) for message in self.list_template.data
+        if self._list_template.load_json():
+            self._sequence = [
+                ChatModelResponse(**message) for message in self._list_template.data
             ]
             return True
         return False
@@ -138,9 +182,11 @@ class SequenceManager(Protocol):
         Returns:
             bool: True if saving was successful, False on error.
         """
-        if self.sequence:
-            data: List[ChatModelResponse] = [dict(message) for message in self.sequence]
-            return self.list_template.save_json(data)
+        if self._sequence:
+            data: List[ChatModelResponse] = [
+                dict(message) for message in self._sequence
+            ]
+            return self._list_template.save_json(data)
         return False
 
     def _append_single_message(self, message: ChatModelResponse) -> None:
@@ -150,7 +196,7 @@ class SequenceManager(Protocol):
         Args:
             message (ChatModelResponse): The ChatModelResponse to append.
         """
-        self.sequence.append(message)
+        self._sequence.append(message)
 
     def _append_multiple_messages(self, messages: List[ChatModelResponse]) -> None:
         """
