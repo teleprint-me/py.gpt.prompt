9f7e0c1da4b2fa80abc6aa7fd05f07e639e55957
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-27 22:29:18 -0400

    feat: Add FunctionManager for processing function responses
    - Added the `FunctionManager` class responsible for processing function responses.
    - This manager handles the execution of functions, generation of function response messages, and enqueueing them in the session manager.
    - It provides a mechanism to process function responses within the chat model context.
    
    This addition introduces support for handling and processing function output in the chat model.

---
+++ pygptprompt/function/manager.py
@@ -0,0 +1,48 @@
+"""
+pygptprompt/function/manager.py
+"""
+from pygptprompt.config.manager import ConfigurationManager
+from pygptprompt.function.factory import FunctionFactory
+from pygptprompt.model.sequence.session_manager import SessionManager
+from pygptprompt.pattern.model import ChatModel, ChatModelResponse
+
+
+class FunctionManager:
+    def __init__(
+        self,
+        function_factory: FunctionFactory,
+        config: ConfigurationManager,
+        chat_model: ChatModel,
+    ):
+        self.function_factory = function_factory
+        self.logger = config.get_logger(
+            "app.log.general", self.__class__.__name__, "DEBUG"
+        )
+        self.chat_model = chat_model
+
+    def process_function(
+        self,
+        assistant_message: ChatModelResponse,
+        session_manager: SessionManager,
+    ) -> bool:
+        if assistant_message["role"] != "function":
+            return False  # not a function, do nothing
+
+        function_result = self.function_factory.execute_function(assistant_message)
+        if function_result is None:
+            self.logger.error(
+                f"Function {self.function_factory.function_name} did not return a result."
+            )
+            return False
+
+        function_message = self.function_factory.query_function(
+            chat_model=self.chat_model,
+            function_result=function_result,
+            messages=session_manager.output(),
+        )
+        if function_message is None:
+            self.logger.error("Failed to generate a function response message.")
+            return False
+
+        session_manager.enqueue(message=function_message)
+        return True  # successfully processed function
