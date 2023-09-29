29429272c6c661a2de9ac5eeece225965654629a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-28 22:34:52 -0400

    feat: Added new source file for lazy function mapping
    - Created a new Python source file for lazy function mapping
    - Implemented LazyFunctionWrapper and LazyFunctionMapper classes
    - Added docstrings following PEP 257 guidelines
    
    This commit introduces a new Python source file, 'lazy.py,' which contains the implementation of the LazyFunctionWrapper and LazyFunctionMapper classes. These classes facilitate lazy loading of functions and classes in the codebase. Additionally, docstrings have been added to enhance code documentation and adherence to PEP 257 guidelines.

---
+++ pygptprompt/function/lazy.py
@@ -0,0 +1,155 @@
+"""
+pygptprompt/function/lazy.py
+
+This module defines classes and functions related to lazy loading of functions and classes.
+
+Example:
+    from pygptprompt.function.lazy import LazyFunctionMapper
+    from pygptprompt.function.memory import ChatModelMemory
+
+    mapper = LazyFunctionMapper()
+
+    mapper.register_class(
+        "ChatModelMemory",
+        ChatModelMemory,
+        db_name="database/chat_model_static_memory.sqlite",
+        table_name="test",
+    )
+
+    mapper.map_class_methods("ChatModelMemory", ["query_memory", "update_memory"])
+    print(mapper.functions)
+
+
+    update_memory = mapper.functions["ChatModelMemory_update_memory"]
+    print(update_memory("test", "this is just a test"))
+
+    query_memory = mapper.functions["ChatModelMemory_query_memory"]
+    print(query_memory("test"))
+"""
+from typing import Any, Callable, Dict, Type
+
+from pygptprompt.function.memory import ChatModelMemory
+from pygptprompt.function.weather import get_current_weather
+
+
+class LazyFunctionWrapper:
+    """
+    Wrapper class for lazily initializing a class instance.
+
+    This class is used to defer the initialization of an instance of a class until it is actually needed.
+
+    Args:
+        cls (Type[Any]): The class to be instantiated.
+        *args: Positional arguments to be passed to the class constructor.
+        **kwargs: Keyword arguments to be passed to the class constructor.
+
+    Attributes:
+        cls (Type[Any]): The class to be instantiated.
+        args: Positional arguments to be passed to the class constructor.
+        kwargs: Keyword arguments to be passed to the class constructor.
+        instance: The instance of the class once it is initialized.
+    """
+
+    def __init__(self, cls, *args, **kwargs):
+        self.cls = cls
+        self.args = args
+        self.kwargs = kwargs
+        self.instance = None
+
+    def __call__(self, *args, **kwargs):
+        """
+        Call method to lazily initialize and return the class instance.
+
+        Args:
+            *args: Positional arguments to be passed to the class constructor.
+            **kwargs: Keyword arguments to be passed to the class constructor.
+
+        Returns:
+            Any: The instance of the class.
+        """
+        if self.instance is None:
+            self.instance = self.cls(*self.args, **self.kwargs)
+        return self.instance
+
+
+class LazyFunctionMapper:
+    """
+    Mapper class for managing lazy loading of functions and classes.
+
+    This class allows you to register functions and classes for lazy loading and provides methods
+    for instantiating classes and mapping class methods to functions.
+
+    Attributes:
+        _functions (Dict[str, Callable]): A dictionary mapping function names to their corresponding functions.
+        _classes (Dict[str, Type[Any]]): A dictionary mapping class names to their corresponding classes.
+        _class_configurations (Dict[str, LazyFunctionWrapper]): A dictionary mapping class names to LazyFunctionWrapper instances.
+    """
+
+    def __init__(self):
+        self._functions = {"get_current_weather": get_current_weather}
+        self._classes = {"ChatModelMemory": ChatModelMemory}
+        self._class_configurations: Dict[str, LazyFunctionWrapper] = {}
+
+    @property
+    def functions(self) -> Dict[str, Callable]:
+        """
+        Property to access the registered functions.
+
+        Returns:
+            Dict[str, Callable]: A dictionary mapping function names to their corresponding functions.
+        """
+        return self._functions
+
+    def register_function(self, function_name: str, function: Callable) -> None:
+        """
+        Register a function for lazy loading.
+
+        Args:
+            function_name (str): The name to be used for the registered function.
+            function (Callable): The function to be registered.
+        """
+        self._functions[function_name] = function
+
+    def register_class(self, class_name: str, cls: Type[Any], **init_params) -> None:
+        """
+        Register a class for lazy loading.
+
+        Args:
+            class_name (str): The name to be used for the registered class.
+            cls (Type[Any]): The class to be registered.
+            **init_params: Keyword arguments to be passed to the class constructor.
+        """
+        self._classes[class_name] = cls
+        self._class_configurations[class_name] = LazyFunctionWrapper(cls, **init_params)
+
+    def instantiate_class(self, class_name: str):
+        """
+        Instantiate a class registered for lazy loading.
+
+        Args:
+            class_name (str): The name of the registered class to be instantiated.
+
+        Returns:
+            Any: The instance of the class.
+
+        Raises:
+            ValueError: If the specified class name is not registered.
+        """
+        if class_name in self._class_configurations:
+            return self._class_configurations[class_name]()
+        else:
+            raise ValueError(f"No registered class with the name {class_name}")
+
+    def map_class_methods(self, class_name: str, methods: list[str]) -> None:
+        """
+        Map methods of a registered class to functions for lazy loading.
+
+        Args:
+            class_name (str): The name of the registered class.
+            methods (list[str]): A list of method names to be mapped to functions.
+        """
+        instance = self.instantiate_class(class_name)
+        for method_name in methods:
+            method = getattr(instance, method_name)
+            if callable(method):
+                self._functions[f"{class_name}_{method_name}"] = method
