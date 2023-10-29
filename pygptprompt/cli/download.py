80a16421dadc8bd9446fc14015838a87b5038f19
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-10-29 13:13:24 -0400

    refactor: Add huggingface `token` parameter
    - Remove huggingface hub `login` from import and `main`.
    - Add `token` parameter to `download_files` and `download_model`.
    - Add `token` flag in `main` and set default to `False`.
    
    This commit ensures the flexibility and extensibility of using the
    login `token` within the `huggingface_hub` API.

--- pygptprompt/cli/download.py
+++ pygptprompt/cli/download.py
@@ -4,9 +4,10 @@ pygptprompt/cli/download.py
 import logging
 import os
 import sys
+from typing import List, Union
 
 import click
-from huggingface_hub import hf_hub_download, login, model_info
+from huggingface_hub import hf_hub_download, model_info
 from huggingface_hub.hf_api import HfApi
 from huggingface_hub.utils import (
     EntryNotFoundError,
@@ -20,20 +21,30 @@ from pygptprompt.pattern.logger import get_default_logger
 logger = get_default_logger("Download", logging.INFO)
 
 
-def download_files(repo_id, local_dir, file_names) -> None:
+def download_files(
+    repo_id: str,
+    local_dir: str,
+    file_names: List[str],
+    token: Union[bool, str],
+) -> None:
     os.makedirs(local_dir, exist_ok=True)
     for file_name in file_names:
         model_path = hf_hub_download(
             repo_id=repo_id,
             filename=file_name,
             local_dir=local_dir,
-            resume_download=True,
             local_dir_use_symlinks=False,
+            resume_download=True,
+            token=token,
         )
         logger.info(f"Downloaded {file_name} to {model_path}")
 
 
-def download_model(repo_id, local_dir) -> None:
+def download_model(
+    repo_id: str,
+    local_dir: str,
+    token: Union[bool, str],
+) -> None:
     logger.info(f"Using {repo_id} to download model data.")
     retries = 0
     max_retries = 3
@@ -41,7 +52,7 @@ def download_model(repo_id, local_dir) -> None:
     try:
         metadata = model_info(repo_id)
         file_names = [x.rfilename for x in metadata.siblings]
-        download_files(repo_id, local_dir, file_names)
+        download_files(repo_id, local_dir, file_names, token)
     except (EntryNotFoundError, RepositoryNotFoundError) as e:
         logger.error(f"Error downloading model: {e}")
         sys.exit(1)
@@ -50,7 +61,7 @@ def download_model(repo_id, local_dir) -> None:
         while retries < max_retries and HfApi().is_online():
             logger.info("Retrying download...")
             retries += 1
-            download_files(repo_id, local_dir, file_names)
+            download_files(repo_id, local_dir, file_names, token)
     except Exception as e:
         logger.error(f"Error downloading model: {e}")
         sys.exit(1)
@@ -65,12 +76,13 @@ def download_model(repo_id, local_dir) -> None:
     help="The directory where you want to store the downloaded files.",
 )
 def main(repo_id, local_dir, config_path):
+    token = False
+
     if config_path:
         config = ConfigurationManager(config_path, logger=logger)
         token = config.get_environment("HUGGINGFACE_TOKEN")
-        login(token)
 
-    download_model(repo_id, local_dir)
+    download_model(repo_id, local_dir, token)
 
 
 if __name__ == "__main__":
