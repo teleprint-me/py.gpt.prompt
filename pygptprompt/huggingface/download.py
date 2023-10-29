2286d14320fe5e7ae70797c9ae8fbd485126f041
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-10-29 13:44:39 -0400

    feat: Add support for huggingface downloads
    - Add the `HuggingFaceDownload` class for supporting downloading of
      huggingface repositories.
    
    This commit enhances the PyGPTPrompt library by adding support for
    downloading repositories directly from HuggingFace.

---
+++ pygptprompt/huggingface/download.py
@@ -0,0 +1,99 @@
+"""
+pygptprompt/huggingface_hub/download.py
+
+TODO: Work in progress.
+"""
+import logging
+import os
+import sys
+from logging import Logger
+from typing import List, Optional
+
+from huggingface_hub import hf_hub_download, model_info
+from huggingface_hub.hf_api import HfApi
+from huggingface_hub.utils import (
+    EntryNotFoundError,
+    LocalEntryNotFoundError,
+    RepositoryNotFoundError,
+)
+
+from pygptprompt.config.manager import ConfigurationManager
+from pygptprompt.pattern.logger import get_default_logger
+
+
+class HuggingFaceDownload:
+    def __init__(self, config: ConfigurationManager, logger: Optional[Logger] = None):
+        self.config = config
+        self.token = config.get_environment("HUGGINGFACE_TOKEN")
+
+        if logger:
+            self._logger = logger
+        else:
+            self._logger = get_default_logger(self.__class__.__name__, logging.INFO)
+
+    def download_file(
+        self,
+        repo_id: str,
+        local_dir: str,
+        file_name: List[str],
+        repo_type: Optional[str] = None,
+    ) -> str:
+        model_path = hf_hub_download(
+            repo_id=repo_id,
+            repo_type=repo_type,
+            filename=file_name,
+            local_dir=local_dir,
+            local_dir_use_symlinks=False,
+            resume_download=True,
+            token=self.token,
+        )
+        self._logger.info(f"Downloaded {file_name} to {model_path}")
+        return model_path
+
+    def download_all_files(
+        self,
+        repo_id: str,
+        local_dir: str,
+        file_names: List[str],
+        repo_type: Optional[str] = None,
+    ) -> List[str]:
+        model_paths = []
+        os.makedirs(local_dir, exist_ok=True)
+        for file_name in file_names:
+            model_path = self.download_file(
+                repo_id=repo_id,
+                repo_type=repo_type,
+                filename=file_name,
+                local_dir=local_dir,
+                local_dir_use_symlinks=False,
+                resume_download=True,
+            )
+            model_paths.append(model_path)
+        return model_paths
+
+    def download_repository(
+        self,
+        repo_id: str,
+        local_dir: str,
+        repo_type: Optional[str] = None,
+    ) -> None:
+        self._logger.info(f"Using {repo_id} to download source data.")
+        retries = 0
+        max_retries = 3
+
+        try:
+            metadata = model_info(repo_id)
+            file_names = [x.rfilename for x in metadata.siblings]
+            self.download_all_files(repo_id, local_dir, file_names, repo_type)
+        except (EntryNotFoundError, RepositoryNotFoundError) as e:
+            self._logger.error(f"Error downloading source: {e}")
+            sys.exit(1)
+        except LocalEntryNotFoundError as e:
+            self._logger.error(f"Error accessing source: {e}")
+            while retries < max_retries and HfApi().is_online():
+                self._logger.info("Retrying download...")
+                retries += 1
+                self.download_all_files(repo_id, local_dir, file_names, repo_type)
+        except Exception as e:
+            self._logger.error(f"Error downloading source: {e}")
+            sys.exit(1)
