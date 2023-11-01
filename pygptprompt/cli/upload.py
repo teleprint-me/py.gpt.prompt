ec7764d9f50445b0124483d7c18f80276df0c869
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-01 01:22:58 -0400

    feat: Add Hugging Face Upload Capabilities
    - Implemented `upload_file` and `upload_folder` functions to handle individual and bulk uploads.
    - Introduced a `main` function that uses `pathlib.Path` to automatically determine whether the given path is a file or a directory.
    - Added appropriate error handling and logging.
    
    This commit enhances the CLI to support the uploading of files or entire folders to a Hugging Face repository.

--- pygptprompt/cli/upload.py
+++ pygptprompt/cli/upload.py
@@ -4,19 +4,20 @@ pygptprompt/cli/upload.py
 Docs:
     # NOTE: Manual model uploading is limited and error prone.
     - https://huggingface.co/docs/hub/models-uploading
-    # NOTE: Automated model uploading is recommended for larger models.
+    # NOTE: Automated uploading is recommended for larger models.
     - https://huggingface.co/docs/huggingface_hub/guides/upload
     - https://huggingface.co/docs/huggingface_hub/v0.17.1/en/package_reference/login#huggingface_hub.login
 
 User Access Token:
     - https://huggingface.co/settings/tokens
 
-NOTE: This is still a Work in Progress and is mostly a copy and past of the download script. Modifications are still required in order for it to operate as expected.
+NOTE: This is still a Work in Progress.
 """
 import sys
+from pathlib import Path
+from typing import Optional, Union
 
 import click
-from huggingface_hub import login, logout, model_info
 from huggingface_hub.hf_api import HfApi
 from huggingface_hub.utils import (
     EntryNotFoundError,
@@ -25,65 +26,97 @@ from huggingface_hub.utils import (
 )
 
 from pygptprompt import logging
+from pygptprompt.config.manager import ConfigurationManager
 
 
-def upload_file():
-    # Once you’ve created a repository with create_repo(),
-    # you can upload a file to your repository using upload_file().
-    # api = HfApi()
-    # api.create_repo
-    # api.upload_file(
-    #     path_or_fileobj="/path/to/local/folder/README.md",
-    #     path_in_repo="README.md",
-    #     repo_id="username/test-dataset",
-    #     repo_type="dataset",
-    # )
-    ...
-
-
-def upload_folder(repo_id, local_dir, file_names):
-    # api = HfApi()
-    # Upload all the content from the local folder to your remote Space.
-    # By default, files are uploaded at the root of the repo
-    # api.upload_folder(
-    #     folder_path="/path/to/local/space",
-    #     repo_id="username/my-cool-space",
-    #     repo_type="space",
-    # )
-    ...
-
-
-def upload_model(repo_id, local_dir) -> str:
-    logging.info(f"Using {repo_id} to download model data.")
-    retries = 0
-    max_retries = 3
-
+def upload_file(
+    api: HfApi,
+    repo_id: str,
+    local_file: Union[str, Path],
+    repo_path: str,
+    token: Optional[str] = None,
+    repo_type: Optional[str] = None,
+):
+    """
+    Upload a file to a specific Hugging Face repository.
+    """
     try:
-        metadata = model_info(repo_id)
-        file_names = [x.rfilename for x in metadata.siblings]
-        download_files(repo_id, local_dir, file_names)
-    except (EntryNotFoundError, RepositoryNotFoundError) as e:
-        logging.error(f"Error downloading model: {e}")
-        sys.exit(1)
-    except LocalEntryNotFoundError as e:
-        logging.error(f"Error accessing model: {e}")
-        while retries < max_retries and HfApi().is_online():
-            logging.info("Retrying download...")
-            retries += 1
-            download_files(repo_id, local_dir, file_names)
+        api.upload_file(
+            path_or_fileobj=local_file,
+            path_in_repo=repo_path,
+            repo_id=repo_id,
+            token=token,
+            repo_type=repo_type,
+        )
+        logging.info(f"Successfully uploaded {local_file} to {repo_id}")
     except Exception as e:
-        logging.error(f"Error downloading model: {e}")
+        logging.error(f"Error uploading file: {e}")
         sys.exit(1)
+
+
+def upload_folder(
+    api,
+    repo_id: str,
+    local_dir: Union[str, Path],
+    token: Optional[str] = None,
+    repo_type: Optional[str] = None,
+) -> None:
+    """
+    Upload all files from a local directory to a specific Hugging Face repository.
+    """
+    try:
+        api.upload_folder(
+            repo_id=repo_id,
+            folder_path=local_dir,
+            token=token,
+            repo_type=repo_type,
+        )
+        logging.info(f"Successfully uploaded folder {local_dir} to {repo_id}")
+    except Exception as e:
+        logging.error(f"Error uploading folder: {e}")
 
 
 @click.command()
-@click.option("--repo_id", help="The Hugging Face repository ID.")
+@click.option("-c", "--config_path", help="The configuration file path.")
+@click.option("-r", "--repo_id", help="The Hugging Face repository ID.")
 @click.option(
-    "--local_dir", help="The directory where you want to store the downloaded files."
+    "-t",
+    "--repo_type",
+    help="The type of repository. `model`, `dataset`, or `space`. defaults to `None`.",
 )
-def main(repo_id, local_dir):
-    upload_model(repo_id, local_dir)
+@click.option(
+    "-p",
+    "--path",
+    help="The path where you want to read the uploaded files or directories from.",
+)
+def main(repo_id, repo_type, path, config_path):
+    config = None
 
+    if config_path:
+        config = ConfigurationManager(config_path)
+        token = config.get_environment("HUGGINGFACE_TOKEN")
 
-if __name__ == "__main__":
-    main()
+    api = HfApi()
+    api.create_repo(repo_id, exist_ok=True, repo_type=repo_type)
+
+    path_obj = Path(path)
+    if path_obj.is_file():
+        upload_file(
+            api,
+            repo_id,
+            str(path_obj),
+            repo_path=str(path_obj.name),
+            token=token,
+            repo_type=repo_type,
+        )
+    elif path_obj.is_dir():
+        upload_folder(
+            api,
+            repo_id,
+            str(path_obj),
+            token=token,
+            repo_type=repo_type,
+        )
+    else:
+        logging.error("The specified path is neither a file nor a directory.")
+        sys.exit(1)
