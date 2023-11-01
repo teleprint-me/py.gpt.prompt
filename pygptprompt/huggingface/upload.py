58cd16b0652d251e4a5d8d6e38d85a3f2acd9d8a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-01 11:36:19 -0400

    feat: Implement HuggingFaceUpload class for model upload
    - Added a new HuggingFaceUpload class to handle the uploading of models or folders to Hugging Face repositories.
    - Introduced methods for file and folder uploads, as well as an `upload` method to determine the type of upload needed.
    - Integrated logging and configuration management into the class.
    
    This class simplifies the uploading process and aligns with the existing HuggingFaceDownload class.

--- pygptprompt/huggingface/upload.py
+++ pygptprompt/huggingface/upload.py
@@ -1,5 +1,110 @@
 """
-pygptprompt/huggingface_hub/upload.py
+pygptprompt/huggingface/upload.py
 
-TODO: Work in progress.
+Docs:
+    # NOTE: Manual model uploading is limited and error prone.
+    - https://huggingface.co/docs/hub/models-uploading
+    # NOTE: Automated uploading is recommended for larger models.
+    - https://huggingface.co/docs/huggingface_hub/guides/upload
+    - https://huggingface.co/docs/huggingface_hub/v0.17.1/en/package_reference/login#huggingface_hub.login
+
+User Access Token:
+    - https://huggingface.co/settings/tokens
+
+NOTE: This is still a Work in Progress.
 """
+import logging
+import sys
+from logging import Logger
+from pathlib import Path
+from typing import Optional, Union
+
+from huggingface_hub.hf_api import HfApi
+from huggingface_hub.utils import (
+    EntryNotFoundError,
+    LocalEntryNotFoundError,
+    RepositoryNotFoundError,
+)
+
+from pygptprompt.config.manager import ConfigurationManager
+from pygptprompt.pattern.logger import get_default_logger
+
+
+class HuggingFaceUpload:
+    def __init__(
+        self,
+        config: Optional[ConfigurationManager] = None,
+        logger: Optional[Logger] = None,
+    ):
+        self.api = HfApi()
+        self.token = None
+
+        if config:
+            self.token = config.get_environment("HUGGINGFACE_TOKEN")
+
+        if logger:
+            self._logger = logger
+        else:
+            self._logger = get_default_logger(self.__class__.__name__, logging.INFO)
+
+    def upload_file(
+        self,
+        local_file: Union[str, Path],
+        repo_path: str,
+        repo_id: str,
+        repo_type: Optional[str] = None,
+    ):
+        try:
+            self.api.upload_file(
+                path_or_fileobj=local_file,
+                path_in_repo=repo_path,
+                repo_id=repo_id,
+                token=self.token,
+                repo_type=repo_type,
+            )
+            self._logger.info(f"Successfully uploaded {local_file} to {repo_id}")
+        except Exception as e:
+            self._logger.error(f"Error uploading file: {e}")
+            sys.exit(1)
+
+    def upload_folder(
+        self,
+        local_path: Union[str, Path],
+        repo_id: str,
+        repo_type: Optional[str] = None,
+    ):
+        try:
+            self.api.upload_folder(
+                repo_id=repo_id,
+                folder_path=local_path,
+                token=self.token,
+                repo_type=repo_type,
+            )
+            self._logger.info(f"Successfully uploaded folder {local_path} to {repo_id}")
+        except Exception as e:
+            self._logger.error(f"Error uploading folder: {e}")
+
+    def upload(
+        self,
+        path: Union[str, Path],
+        repo_id: str,
+        repo_type: Optional[str] = None,
+    ):
+        path_obj = Path(path)
+
+        if path_obj.is_file():
+            self.upload_file(
+                local_file=str(path_obj),
+                repo_path=str(path_obj.name),
+                repo_id=repo_id,
+                repo_type=repo_type,
+            )
+        elif path_obj.is_dir():
+            self.upload_folder(
+                local_path=str(path_obj),
+                repo_id=repo_id,
+                repo_type=repo_type,
+            )
+        else:
+            self._logger.error("The specified path is neither a file nor a directory.")
+            sys.exit(1)
