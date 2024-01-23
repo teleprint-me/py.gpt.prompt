20fb8984f6eeedd5cb20d296aa311742d9b23ba9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-01-22 21:57:28 -0500

    refactor: Improved PDF Processing and Logging
    - Updated the function signature of the 'main' function to accept more descriptive argument names and added support for viewing raw text chunks.
    - Improved input validation for the PDF file path.
    - Added logging support with a logger instance for better debugging.
    - Refactored the code for configuration, chat model creation, and PDF processing, enhancing code organization and maintainability.

--- pygptprompt/cli/pdf.py
+++ pygptprompt/cli/pdf.py
@@ -2,97 +2,130 @@
 pygptprompt/cli/pdf.py
 """
 import os
+from logging import Logger
+from pathlib import Path
+from typing import Dict, List, Union
 
 import click
 
 from pygptprompt.config.manager import ConfigurationManager
 from pygptprompt.model.factory import ChatModelFactory
-from pygptprompt.processor.pdf import PDFProcessor
+from pygptprompt.processor.pdf import PDFPage, PDFPageChunk, PDFProcessor
 
 
 @click.command()
 @click.argument(
-    "path_config",
+    "config_path",
     type=click.Path(exists=True),
+    metavar="CONFIG_PATH",
+)
+@click.argument(
+    "pdf_path",
+    type=click.Path(exists=True),
+    metavar="PDF_PATH",
 )
 @click.option(
-    "--path_input",
+    "-o",
+    "--output",
     type=click.STRING,
-    default=str(),
-    help="The path to the source document. Default is empty string.",
+    default="",
+    metavar="OUTPUT_PATH",
+    help="Path to the output text file. (default: empty)",
 )
 @click.option(
-    "--path_output",
+    "-s",
+    "--session-name",
     type=click.STRING,
-    default=str(),
-    help="The path to the source document. Default is empty string.",
+    default="",
+    metavar="SESSION_LABEL",
+    help="Specify a session label for the database collection and JSON files. (default: empty)",
 )
 @click.option(
-    "--path_context",
-    type=click.STRING,
-    default=str(),
-    help="The path to the source document. Default is empty string.",
-)
-@click.option(
-    "--chunk_size",
+    "--max-chunk-length",
     type=click.INT,
     default=256,
-    help="The maximum length of each chunk.",
+    metavar="CHUNK_LENGTH",
+    help="The maximum length of each text chunk. (default: 256)",
 )
 @click.option(
     "--provider",
     type=click.STRING,
     default="llama_cpp",
-    help="The provider for the chat model. llama_cpp or openai. default is llama_cpp.",
+    metavar="PROVIDER",
+    help="The provider for the chat model. Options: llama_cpp, openai. (default: llama_cpp)",
 )
-def main(path_input, path_output, chunk_size, provider, config_path):
+@click.option(
+    "--view-chunks",
+    is_flag=True,
+    help="View the raw text chunks without processing by the model.",
+)
+def main(
+    config_path,
+    pdf_path,
+    output,
+    session_name,
+    max_chunk_length,
+    provider,
+    view_chunks,
+):
     """
-    Convert a PDF document into text and print the text of the first page.
+    Convert a PDF document into text and print or save the processed text.
     """
-    if not path_input:
-        print("A valid file path must be given as an argument.")
-        exit(1)
-    if not os.path.isfile(path_input) or not path_input.endswith(".pdf"):
-        print("The path must point to a valid PDF file.")
-        exit(1)
+    # Check if the input PDF file exists
+    if not os.path.isfile(pdf_path) or not pdf_path.endswith(".pdf"):
+        click.echo("Error: The provided PDF path is invalid or does not exist.")
+        return
 
+    # Load configuration
     config = ConfigurationManager(config_path)
+
+    # Setup logger
+    logger: Logger = config.get_logger("general", Path(__file__).stem)
+
+    # Create the chat model
     chat_model_factory = ChatModelFactory(config)
     chat_model = chat_model_factory.create_model(provider)
 
+    # Create the PDF processor
     processor = PDFProcessor(
-        path_input,
-        path_output,
-        chunk_size,
+        session_name,
+        max_chunk_length,
         provider,
         config,
         chat_model,
     )
 
-    pages = processor.convert_pdf_to_text()
+    # Convert PDF to text
+    pages: List[PDFPage] = processor.convert_pdf_to_text()
+    pages = processor.convert_pages_to_chunks(pages, max_chunk_length)
 
-    if path_output:
-        with open(path_output, "a+") as text_file:
+    if output:
+        # Save the processed text to the specified output file
+        with open(output, "a+") as text_file:
             for page in pages:
                 text_file.write(page)
+    elif view_chunks:
+        # View the raw text chunks without processing by the model
+        click.echo("--- Raw Text Chunks ---")
+        for index, page in enumerate(pages):
+            click.echo(f"Page {index + 1} Chunks:")
+            for chunk in page["chunks"]:
+                click.echo(chunk)
+                click.echo("---")
     else:
-        chunks = []
-        for page in pages:
+        # Print the processed text chunks
+        click.echo("--- Processed Text Chunks ---")
+        for index, page in enumerate(pages):
             # Process the text chunks with the chat model
             processed_chunks, metadata = processor.chunk_text_with_chat_model(
                 page["text"], page["metadata"]
             )
-            chunks.extend(processed_chunks)
-
-            # Update metadata with the number of chunks
             page["metadata"] = metadata
 
-        # Print the processed chunks
-        print("---")
-        for index, chunk in enumerate(chunks):
-            print(f"Chunk {index + 1}:")
-            print(chunk)
-            print("---")
+            click.echo(f"Page {index + 1} Chunks:")
+            for chunk in processed_chunks:
+                click.echo(chunk)
+                click.echo("---")
 
 
 if __name__ == "__main__":
