5c5cf006f8acb35f83855a1dad6baf4587264053
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-03-09 23:58:03 -0500

    chore: Apply formatting

--- pygptprompt/gguf/cli/convert-endian.py
+++ pygptprompt/gguf/cli/convert-endian.py
@@ -8,12 +8,6 @@ from pathlib import Path
 
 import numpy as np
 
-# Necessary to load the local gguf package
-if "NO_LOCAL_GGUF" not in os.environ and (Path(__file__).parent.parent.parent / 'gguf-py').exists():
-    sys.path.insert(0, str(Path(__file__).parent.parent))
-
-import gguf
-
 
 def convert_byteorder(reader: gguf.GGUFReader, args: argparse.Namespace) -> None:
     if np.uint32(1) == np.uint32(1).newbyteorder("<"):
@@ -29,7 +23,9 @@ def convert_byteorder(reader: gguf.GGUFReader, args: argparse.Namespace) -> None
     else:
         file_endian = host_endian
     order = host_endian if args.order == "native" else args.order
-    print(f"* Host is {host_endian.upper()} endian, GGUF file seems to be {file_endian.upper()} endian")
+    print(
+        f"* Host is {host_endian.upper()} endian, GGUF file seems to be {file_endian.upper()} endian"
+    )
     if file_endian == order:
         print(f"* File is already {order.upper()} endian. Nothing to do.")
         sys.exit(0)
@@ -40,23 +36,33 @@ def convert_byteorder(reader: gguf.GGUFReader, args: argparse.Namespace) -> None
             gguf.GGMLQuantizationType.F16,
             gguf.GGMLQuantizationType.Q8_0,
         ):
-            raise ValueError(f"Cannot handle type {tensor.tensor_type.name} for tensor {repr(tensor.name)}")
+            raise ValueError(
+                f"Cannot handle type {tensor.tensor_type.name} for tensor {repr(tensor.name)}"
+            )
     print(f"* Preparing to convert from {file_endian.upper()} to {order.upper()}")
     if args.dry_run:
         return
     print("\n*** Warning *** Warning *** Warning **")
     print("* This conversion process may damage the file. Ensure you have a backup.")
     if order != host_endian:
-        print("* Requested endian differs from host, you will not be able to load the model on this machine.")
-    print("* The file will be modified immediately, so if conversion fails or is interrupted")
-    print("* the file will be corrupted. Enter exactly YES if you are positive you want to proceed:")
+        print(
+            "* Requested endian differs from host, you will not be able to load the model on this machine."
+        )
+    print(
+        "* The file will be modified immediately, so if conversion fails or is interrupted"
+    )
+    print(
+        "* the file will be corrupted. Enter exactly YES if you are positive you want to proceed:"
+    )
     response = input("YES, I am sure> ")
     if response != "YES":
         print("You didn't enter YES. Okay then, see ya!")
         sys.exit(0)
     print(f"\n* Converting fields ({len(reader.fields)})")
     for idx, field in enumerate(reader.fields.values()):
-        print(f"- {idx:4}: Converting field {repr(field.name)}, part count: {len(field.parts)}")
+        print(
+            f"- {idx:4}: Converting field {repr(field.name)}, part count: {len(field.parts)}"
+        )
         for part in field.parts:
             part.byteswap(inplace=True)
     print(f"\n* Converting tensors ({len(reader.tensors)})")
@@ -79,7 +85,7 @@ def convert_byteorder(reader: gguf.GGUFReader, args: argparse.Namespace) -> None
         for block_num in range(n_blocks):
             block_offs = block_num * block_size
             # I know I said f16, but it doesn't matter here - any simple 16 bit type works.
-            delta = tensor.data[block_offs:block_offs + 2].view(dtype=np.uint16)
+            delta = tensor.data[block_offs : block_offs + 2].view(dtype=np.uint16)
             delta.byteswap(inplace=True)
             if block_num % 100000 == 0:
                 print(f"[{(n_blocks - block_num) // 1000}K]", end="")
@@ -91,20 +97,24 @@ def convert_byteorder(reader: gguf.GGUFReader, args: argparse.Namespace) -> None
 def main() -> None:
     parser = argparse.ArgumentParser(description="Convert GGUF file byte order")
     parser.add_argument(
-        "model", type=str,
+        "model",
+        type=str,
         help="GGUF format model filename",
     )
     parser.add_argument(
-        "order", type=str, choices=['big', 'little', 'native'],
+        "order",
+        type=str,
+        choices=["big", "little", "native"],
         help="Requested byte order",
     )
     parser.add_argument(
-        "--dry-run", action="store_true",
+        "--dry-run",
+        action="store_true",
         help="Don't actually change anything",
     )
     args = parser.parse_args(None if len(sys.argv) > 1 else ["--help"])
-    print(f'* Loading: {args.model}')
-    reader = gguf.GGUFReader(args.model, 'r' if args.dry_run else 'r+')
+    print(f"* Loading: {args.model}")
+    reader = gguf.GGUFReader(args.model, "r" if args.dry_run else "r+")
     convert_byteorder(reader, args)
 
 
